724540fc53d4e8cce81ea32c63d051cf48be6142
Maria Doyle <maria.doyle@petermac.org>
2020-08-15 12:25:12 +1000

    Document example dataset

---
+++ R/formatted_image.R
@@ -0,0 +1,9 @@
+#' Example Inform image
+#'
+#' A dataset containing an example Inform image, loaded and formatted as described in data-raw/formatted_image.R 
+#'
+#' @format A SingleCellExperiment object where the count assay stores the expression level of every marker (rows) for
+#every cell (columns), and cell phenotype, x and y coordinates are stored
+#under colData
+#' @usage data(formatted_image)
+"formatted_image"
