b6621591b7ad3d376c8a6317b90270ac26186fb0
Maria Doyle <maria.doyle@petermac.org>
2020-08-17 12:47:21 +1000

    Remove FOXP3
    Former-commit-id: c4d40f1c521f0e66eeab5a583eb36ab1e49eefeb [formerly 68c4d46ed6736b424996703e4caf749d1442f627]
    Former-commit-id: 7a2f447e19056d7eb74b6a8846e03f6f6e45e7e5
    Former-commit-id: e109da5576b98b75938b16064594f1dbcfc003a3

--- data-raw/formatted_image.R
+++ data-raw/formatted_image.R
@@ -4,12 +4,16 @@ library(SPIAT)
 
 raw_inform_data <- "S6_[49209,17530]_cell_seg_data.txt"
 
-markers <- c("DAPI","CD3","PDL1","FOXP3","CD4","CD8","AMACR")
+markers <- c("DAPI","CD3","PDL1","CD4","CD8","AMACR")
 
-intensity_columns_interest <- c("Nucleus DAPI (DAPI) Mean (Normalized Counts, Total Weighting)","Cytoplasm CD3 (Opal 520) Mean (Normalized Counts, Total Weighting)", 
-                                "Membrane PDL-1 (Opal 540) Mean (Normalized Counts, Total Weighting)", "Cytoplasm FOXP3 (Opal 570) Mean (Normalized Counts, Total Weighting)", 
-                                "Cytoplasm CD4 (Opal 620) Mean (Normalized Counts, Total Weighting)", "Cytoplasm CD8 (Opal 650) Mean (Normalized Counts, Total Weighting)", 
-                                "Cytoplasm AMACR (Opal 690) Mean (Normalized Counts, Total Weighting)")
+intensity_columns_interest <- c(
+    "Nucleus DAPI (DAPI) Mean (Normalized Counts, Total Weighting)",
+    "Cytoplasm CD3 (Opal 520) Mean (Normalized Counts, Total Weighting)", 
+    "Membrane PDL-1 (Opal 540) Mean (Normalized Counts, Total Weighting)",
+    "Cytoplasm CD4 (Opal 620) Mean (Normalized Counts, Total Weighting)",
+    "Cytoplasm CD8 (Opal 650) Mean (Normalized Counts, Total Weighting)", 
+    "Cytoplasm AMACR (Opal 690) Mean (Normalized Counts, Total Weighting)"
+)
 
 #Formats an INFORM or HALO image into a singlecellexperiment class
 #where the count assay stores the expression level of every marker (rows) for
