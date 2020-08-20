ae0afaf282859971d020124f440734792cd87b21
Maria Doyle <maria.doyle@petermac.org>
2020-08-20 20:26:37 +1000

    Update info for vignette data
    Former-commit-id: 038b4e9da33d13e275171de3bd0c922d5a5233e1 [formerly b56db9ddf5089e1519c5fe752615d17614ad41a0]
    Former-commit-id: 9cc2de5d78dde02261c9f748ea14c1342cc85dd3
    Former-commit-id: d1196d0e7cf76dd45e800fc8d866eff8a5aa3dc9

--- R/formatted_image.R
+++ R/formatted_image.R
@@ -3,7 +3,8 @@
 #' A dataset containing an example Inform image, loaded and formatted as described in data-raw/formatted_image.R 
 #'
 #' @format A SingleCellExperiment object where the count assay stores the expression level of every marker (rows) for
-#every cell (columns), and cell phenotype, x and y coordinates are stored
+#every cell (columns), and cell phenotype, x and y coordinates, other properties (Cell Size, Nucleus Size, 
+#' Nucleus Compactness, Nucleus Axis Ratio, Cell Axis Ratio ) are stored under colData
 #under colData
 #' @usage data(formatted_image)
 "formatted_image"
