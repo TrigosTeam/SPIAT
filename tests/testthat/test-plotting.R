35925c3c038b35a3c6f43cbf4183adde517ff4e3
Maria Doyle <maria.doyle@petermac.org>
2020-08-20 20:51:38 +1000

    Add test for measure_association_to_cell_properties

--- tests/testthat/test-plotting.R
+++ tests/testthat/test-plotting.R
@@ -123,3 +123,12 @@ test_that("marker_surface_plot_stack() creates a plot", {
     expect_is(p, "plotly")
     
 })
+
+
+test_that("measure_association_to_cell_properties() creates a plot", {
+    
+    p <- measure_association_to_cell_properties(formatted_image, phenotypes = c("CD3,CD4", "CD3,CD8"))
+    
+    expect_is(p, "ggplot")
+    
+})
