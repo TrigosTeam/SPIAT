93915b2892efb1f5f9d1d234f8044962af0518be
Maria Doyle <maria.doyle@petermac.org>
2020-08-21 21:50:52 +1000

    Change plot_cell_percentages from colours to grey
    Following the principles outlined in the paper "Effective Visual Communication for the Quantitative Scientist" paper, here https://graphicsprinciples.github.io/
    "Do not use color to differentiate between categories of the same variable"
    
    
    Former-commit-id: 16c841175e0266e19060f83efc69fbd5224bf545

--- R/plot_cell_percentages.R
+++ R/plot_cell_percentages.R
@@ -16,8 +16,8 @@ plot_cell_percentages <- function(cell_proportions, tumour_marker=NULL){
   cell_proportions <- cell_proportions[cell_proportions$Cell_type != "OTHER",]
 
   cell_percentages_full_plot <-
-    ggplot(cell_proportions,aes(x = Cell_type,y = Percentage, fill = Cell_type)) +
-      geom_bar(stat = 'identity') +
+    ggplot(cell_proportions,aes(x = Cell_type,y = Percentage)) +
+      geom_bar(stat = 'identity', fill = "#bababa") +
       theme_classic() +
       theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5), legend.position = "none") +
       xlab("Cell Type") + ylab("Proportion of total cells") +
@@ -30,9 +30,8 @@ plot_cell_percentages <- function(cell_proportions, tumour_marker=NULL){
     cell_proportions_no_tumour$Percentage_label <- round(cell_proportions_no_tumour$Percentage, digits=1)
 
     cell_percentages_no_tumour_plot <-
-      ggplot(cell_proportions_no_tumour, aes(x = Cell_type,y = Percentage,
-                                             fill = Cell_type)) +
-      geom_bar(stat = 'identity') +
+      ggplot(cell_proportions_no_tumour, aes(x = Cell_type,y = Percentage)) +
+      geom_bar(stat = 'identity', fill = "#bababa") +
       ggtitle("Excluding tumor cells")+
       theme_classic() +
       theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5), legend.position = "none") +
