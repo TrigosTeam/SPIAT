09822aa3e4c3ba7b9bd0e9a7882d56f830234981
Maria Doyle <maria.doyle@petermac.org>
2020-08-22 12:40:22 +1000

    Change plot_distance_heatmap to sans serif font
    From the principles here: https://github.com/GraphicsPrinciples/CheatSheet/blob/master/NVSCheatSheet.pdf
    Keep the font style simple – sans serif is easier to read.
    
    
    Former-commit-id: 253b89adc21b568bfa8cb5cc61c821048bb19651 [formerly ce7d9212ebdddec2bb01c76140a100c1b0ca773a]
    Former-commit-id: 8081f706413280b5eba7bac5231187cd958699a2
    Former-commit-id: 09f7c45777b6b986abfa5e6f94c2458ea200a3fd

--- R/plot_distance_heatmap.R
+++ R/plot_distance_heatmap.R
@@ -19,7 +19,7 @@ plot_distance_heatmap <- function(phenotype_distances_result, metric = "mean"){
         xlab("Cell of interest (COI)") +
         ylab("Nearest cell to COI") +
         theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_rect(fill = "white"),
-              axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5), text=element_text(size=14,family="serif")) +
+              axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5), text=element_text(size=14)) +
         scale_fill_viridis_c(limits = limit, direction = -1)
       print(g)
 
@@ -27,7 +27,7 @@ plot_distance_heatmap <- function(phenotype_distances_result, metric = "mean"){
       limit <- range(unlist(phenotype_distances_result$Std.Dev), na.rm=TRUE)
       g <- ggplot(phenotype_distances_result, aes(x = Target, y = Nearest, fill = Std.Dev)) + geom_tile() + xlab("Cell of interest (COI)") + ylab("Nearest cell to COI") +
         theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_rect(fill = "white"),
-              axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5), text=element_text(size=14,family="serif")) +
+              axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5), text=element_text(size=14)) +
         scale_fill_viridis_c(limits = limit, direction = -1)
       print(g)
 
@@ -35,7 +35,7 @@ plot_distance_heatmap <- function(phenotype_distances_result, metric = "mean"){
       limit <- range(unlist(phenotype_distances_result$Median), na.rm=TRUE)
       g <- ggplot(phenotype_distances_result, aes(x = Target, y = Nearest, fill = Median)) + geom_tile() + xlab("Cell of interest (COI)") + ylab("Nearest cell to COI") +
         theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_rect(fill = "white"),
-              axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5), text=element_text(size=14,family="serif")) +
+              axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5), text=element_text(size=14)) +
         scale_fill_viridis_c(limits = limit, direction = -1)
       print(g)
     }
