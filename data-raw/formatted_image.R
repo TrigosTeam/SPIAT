e43c0418d516535954185d6b3c26ae9b0420e4e2
Maria Doyle <maria.doyle@petermac.org>
2020-08-28 20:18:17 +1000

    Allow input of just location of marker instead of having to find and paste column names.
    Updated test data, tests and vignette.
    Note now keeps hyphen in PDL-1 marker name to aid in matching to intensity column in inform format and to be consistent with other marker names with hyphens.
    
    
    Former-commit-id: ddafa39af498d1fe6dfc63c8bf1546a57c976eb0 [formerly ece0b055702bda0fdba0b49d7d80452ca6fa5c52]
    Former-commit-id: 58b988904395bc65588567f1573f735cfa516ff7
    Former-commit-id: 4c4d496069e1375df82bafa80702e6dc982d61c8

--- data-raw/formatted_image.R
+++ data-raw/formatted_image.R
@@ -4,27 +4,16 @@ library(SPIAT)
 
 raw_inform_data <- "S6_[49209,17530]_cell_seg_data.txt"
 
-markers <- c("DAPI","CD3","PDL1","CD4","CD8","AMACR")
+markers <- c("DAPI","CD3","PDL-1","CD4","CD8","AMACR")
 
-intensity_columns_interest <- c(
-    "Nucleus DAPI (DAPI) Mean (Normalized Counts, Total Weighting)",
-    "Cytoplasm CD3 (Opal 520) Mean (Normalized Counts, Total Weighting)", 
-    "Membrane PDL-1 (Opal 540) Mean (Normalized Counts, Total Weighting)",
-    "Cytoplasm CD4 (Opal 620) Mean (Normalized Counts, Total Weighting)",
-    "Cytoplasm CD8 (Opal 650) Mean (Normalized Counts, Total Weighting)", 
-    "Cytoplasm AMACR (Opal 690) Mean (Normalized Counts, Total Weighting)"
-)
+locations <- c("Nucleus", "Cytoplasm", "Membrane", "Cytoplasm", "Cytoplasm", "Cytoplasm")
 
-#Formats an INFORM or HALO image into a singlecellexperiment class
-#where the count assay stores the expression level of every marker (rows) for
-#every cell (columns), and cell phenotype, x and y coordinates, other properties (Cell Size, Nucleus Size, 
-#' Nucleus Compactness, Nucleus Axis Ratio, Cell Axis Ratio ) are stored under colData
-formatted_image <- format_image_to_sce(format="INFORM",
-                                       image=raw_inform_data,
-                                       markers=markers,
-                                       dye_columns_interest=NULL,
-                                       intensity_columns_interest=intensity_columns_interest)
+formatted_image <- format_image_to_sce(
+                          format="INFORM",
+                          image=raw_inform_data,
+                          markers=markers,
+                          locations= locations)
 
-formatted_image <- select_phenotypes(formatted_image, keep=TRUE, phenotypes = c("AMACR", "CD3,CD4", "CD3,CD8", "PDL1"))
+formatted_image <- select_phenotypes(formatted_image, keep=TRUE, phenotypes = c("AMACR", "CD3,CD4", "CD3,CD8", "PDL-1"))
 
 usethis::use_data(formatted_image, overwrite = TRUE)
