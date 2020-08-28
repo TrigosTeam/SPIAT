4c4d496069e1375df82bafa80702e6dc982d61c8
Maria Doyle <maria.doyle@petermac.org>
2020-08-28 20:18:17 +1000

    Allow input of just location of marker instead of having to find and paste column names.
    Updated test data, tests and vignette.
    Note now keeps hyphen in PDL-1 marker name to aid in matching to intensity column in inform format and to be consistent with other marker names with hyphens.
    
    
    Former-commit-id: ece0b055702bda0fdba0b49d7d80452ca6fa5c52
    Former-commit-id: 58b988904395bc65588567f1573f735cfa516ff7

--- tests/testthat/test-calculations.R
+++ tests/testthat/test-calculations.R
@@ -2,7 +2,7 @@ context("calculations")
 
 test_that("calculate_cell_proportions() works", {
     
-    res <- data.frame(Cell_type = c("AMACR", "CD3,CD4", "CD3,CD8", "PDL1"), 
+    res <- data.frame(Cell_type = c("AMACR", "CD3,CD4", "CD3,CD8", "PDL-1"), 
                           Number_of_cells = c(4446, 513, 138, 4),
                           Proportion = c(0.87159381, 0.10056852, 0.02705352, 0.00078416), 
                           Percentage = c(87.159381, 10.056852, 2.705352, 0.078416),
@@ -17,7 +17,7 @@ test_that("calculate_cell_proportions() works", {
 
 test_that("compute_mixing_score() works", {
     
-    score <- compute_mixing_score(formatted_image, reference_marker = "CD4", target_marker = "PDL1")
+    score <- compute_mixing_score(formatted_image, reference_marker = "CD4", target_marker = "PDL-1")
     
     expect_equal(score, 0.009708738)
     
@@ -48,7 +48,7 @@ test_that("average_minimum_distance() works", {
 test_that("calculate_summary_distances_between_phenotypes() works", {
     
     res <- data.frame(Target = c(rep("AMACR", 3)), 
-                      Nearest = c("CD3,CD4", "CD3,CD8", "PDL1"),
+                      Nearest = c("CD3,CD4", "CD3,CD8", "PDL-1"),
                       Mean = c(155.5926, 243.5603, 732.2836), 
                       Std.Dev = c(89.34635, 131.20464, 346.66617),
                       Median = c(139.8624, 222.6488, 686.4146))
@@ -96,11 +96,11 @@ test_that("identify_cell_clusters() works", {
                       Cell.Axis.Ratio = c(1.17, 1.41, 1.83, 2.03),
                       DAPI = c(14.7, 12.8, 12.7, 11),
                       CD3 = c(3.67, 2.39, 1.3, 1.38),
-                      PDL1 = c(0.357, 0.2, 0.114, 0.23),
+                      `PDL-1` = c(0.357, 0.2, 0.114, 0.23),
                       CD4 = c(3.74, 1.22, 1.9, 1.3),
                       CD8 = c(0.319, 0.05, 9.982, 4.254),
                       AMACR = c(0.034, 0.069, 0.514, 0.061),
-                      Cluster = c("Cluster_1", "Cluster_NA", "Cluster_1", "Cluster_1"))
+                      Cluster = c("Cluster_1", "Cluster_NA", "Cluster_1", "Cluster_1"), check.names = FALSE)
     
     clusters <- identify_cell_clusters(formatted_image, phenotypes_of_interest = c("CD3,CD4", "CD3,CD8"),
                                        radius = 30)
@@ -123,11 +123,11 @@ test_that("identify_cell_communities() works", {
                       Cell.Axis.Ratio = c(1, 1.21, 1.09, 1.34), 
                       DAPI = c(17.588, 21.262, 18.951, 18.631), 
                       CD3 = c(0.229, 0.206, 0.226, 0.212), 
-                      PDL1 = c(0.117, 0.103, 0.126, 0.091), 
+                      `PDL-1` = c(0.117, 0.103, 0.126, 0.091), 
                       CD4 = c(1.188, 0.924, 1.367, 1.266), 
                       CD8 = c(0.087, 0.053, 0.037, 0.046), 
                       AMACR = c(2.074, 1.521, 2.462, 1.968), 
-                      Community = c("Community_1", "Community_1", "Community_1", "Community_1"))
+                      Community = c("Community_1", "Community_1", "Community_1", "Community_1"), check.names = FALSE)
     
     communities <- identify_cell_communities(formatted_image, radius=100)
     
@@ -138,7 +138,7 @@ test_that("identify_cell_communities() works", {
 
 test_that("marker_permutation() works", {
     
-    res <- data.frame(row.names = c("CD3", "PDL1", "CD4", "CD8"),
+    res <- data.frame(row.names = c("CD3", "PDL-1", "CD4", "CD8"),
                       Percentage_of_occurrence = c(100, 38, 100, 100),
                       Observed_cell_number = c(0, 4, 0, 0),
                       Average_bootstrap_cell_number = c(72.35, 0.45, 55.3, 13.61),
@@ -157,7 +157,7 @@ test_that("percentage_of_cells_within_radius() works", {
     
     res <- setNames(c(33.33333, 94.73684, 0.00000, 0.00000 ), c("Cell_664","Cell_2760", "Cell_2992", "Cell_3147"))
     
-    percent_cells <- percentage_of_cells_within_radius(formatted_image, reference_phenotypes = "PDL1", target_phenotypes = "AMACR", radius=100)
+    percent_cells <- percentage_of_cells_within_radius(formatted_image, reference_phenotypes = "PDL-1", target_phenotypes = "AMACR", radius=100)
     
     expect_equal(percent_cells, res, tolerance=1e-7)
     
