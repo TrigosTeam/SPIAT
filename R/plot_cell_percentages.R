81cbdd95047f33e5078a8e151bfbbfbe399a9855
Maria Doyle <maria.doyle@petermac.org>
2020-08-29 00:56:44 +1000

    Enable calculating proportions of specified markers
    Former-commit-id: 8c6fe3c73a0a3d06e8bf387045a76966ee18d5d3 [formerly 8aaaf2f9e5d45532d0959e210aa175240ae5b9d5]
    Former-commit-id: 42c5c52ca703fc68d1c1923e5fd89b130ce55be7
    Former-commit-id: e851c3d9ff50da1a5de1eedf69e4248d531ca806

--- R/plot_cell_percentages.R
+++ R/plot_cell_percentages.R
@@ -3,26 +3,29 @@
 #' @description Plots cells proportions as barplots
 #' @param cell_proportions Output from calculate_cell_proportions
 #' @param tumour_marker Tumour marker to exclude if needed
+#' @param celltype_column Name of column to use for y axis. Default is "Proportion_name"
 #' @import ggplot2
 #' @importFrom stats reorder
 #' @export
 
 
-plot_cell_percentages <- function(cell_proportions, tumour_marker=NULL){
+plot_cell_percentages <- function(cell_proportions, tumour_marker=NULL, cellprop_colname="Proportion_name"){
   
   # setting these variables to NULL as otherwise get "no visible binding for global variable" in R check
   Cell_type <- Percentage <- Percentage_label <- NULL
+  
+  cellprop_colname <- ensym(cellprop_colname)
 
   cell_proportions$Percentage_label <- round(cell_proportions$Percentage, digits=1)
   cell_proportions <- cell_proportions[cell_proportions$Cell_type != "OTHER",]
 
   cell_percentages_full_plot <-
-    ggplot(cell_proportions,aes(x = reorder(Cell_type, Percentage), y = Percentage)) +
+    ggplot(cell_proportions,aes(x = reorder({{cellprop_colname}}, Percentage), y = Percentage)) +
       geom_bar(stat = 'identity', fill = "#bababa") +
       theme_bw() +
       theme(legend.position = "none") +
-      xlab("Cell Type") + ylab("Proportion of total cells") +
-      geom_text(aes(label = Percentage_label), position=position_dodge(width=0.9), hjust=-0.25, size = 2) +
+      xlab("Cell Type") + ylab("Proportion of cells") +
+      geom_text(aes(label = Percentage_label), position=position_dodge(width=0.9), hjust=-0.25, size = 3) +
     coord_flip()
   
   if(!is.null(tumour_marker)){
@@ -32,7 +35,7 @@ plot_cell_percentages <- function(cell_proportions, tumour_marker=NULL){
     cell_proportions_no_tumour$Percentage_label <- round(cell_proportions_no_tumour$Percentage, digits=1)
 
     cell_percentages_no_tumour_plot <-
-      ggplot(cell_proportions_no_tumour, aes(x = reorder(Cell_type, Percentage), y = Percentage)) +
+      ggplot(cell_proportions_no_tumour, aes(x = reorder({{cellprop_colname}}, Percentage), y = Percentage)) +
       geom_bar(stat = 'identity', fill = "#bababa") +
       ggtitle("Excluding tumor cells")+
       theme_bw() +
@@ -44,5 +47,3 @@ plot_cell_percentages <- function(cell_proportions, tumour_marker=NULL){
   }
   print(cell_percentages_full_plot)
 }
-
-
