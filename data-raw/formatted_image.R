aa4219de2317ac574988505ddf367778bd323b27
Maria Doyle <maria.doyle@petermac.org>
2020-08-31 11:53:28 +1000

    Change to use full example file
    Former-commit-id: 422d77f31ad0f09c6240c5c9cb792c780f8b10eb [formerly 168e00513cd148c142331ec8a7046e928196bc43]
    Former-commit-id: 0e8f826cf5985c287a83d38ac1d5e0efaa719acf
    Former-commit-id: 30f8d1631da18a96488b4f1500ef48362e0e9d88
    Former-commit-id: 5028dafa6a758c93beb2a8d4c09cd3a81fa492ea

--- data-raw/formatted_image.R
+++ data-raw/formatted_image.R
@@ -3,9 +3,7 @@
 library(SPIAT)
 
 raw_inform_data <- "S6_[49209,17530]_cell_seg_data.txt"
-
 markers <- c("DAPI","CD3","PDL-1","CD4","CD8","AMACR")
-
 locations <- c("Nucleus", "Cytoplasm", "Membrane", "Cytoplasm", "Cytoplasm", "Cytoplasm")
 
 formatted_image <- format_image_to_sce(
@@ -13,7 +11,5 @@ formatted_image <- format_image_to_sce(
                           image=raw_inform_data,
                           markers=markers,
                           locations= locations)
-
-formatted_image <- select_phenotypes(formatted_image, keep=TRUE, phenotypes = c("AMACR", "CD3,CD4", "CD3,CD8", "PDL-1"))
 
 usethis::use_data(formatted_image, overwrite = TRUE)
--- tests/testthat/test-calculations.R
+++ tests/testthat/test-calculations.R
@@ -2,13 +2,17 @@ context("calculations")
 
 test_that("calculate_cell_proportions() works", {
     
-    res <- data.frame(Cell_type = factor(c("AMACR", "CD3,CD4", "CD3,CD8", "PDL-1")), 
-                          Number_of_celltype = c(4446, 513, 138, 4),
-                          Reference = rep("Total", 4),
-                          Number_of_reference = rep(5101, 4),
-                          Proportion = c(0.87159381, 0.10056852, 0.02705352, 0.00078416), 
-                          Percentage = c(87.159381, 10.056852, 2.705352, 0.078416),
-                          Proportion_name = c("AMACR/Total", "CD3,CD4/Total", "CD3,CD8/Total", "PDL-1/Total"),
+    res <- data.frame(row.names = c(1L, 5L, 3L, 4L, 2L, 6L),
+                          Cell_type = factor(c("AMACR", "OTHER", "CD3,CD4", "CD3,CD8", "CD3", "PDL-1")), 
+                          Number_of_celltype = c(4446, 3299, 513, 138, 19, 4),
+                          Reference = rep("Total", 6),
+                          Number_of_reference = rep(8419, 6),
+                          Proportion = c(0.52809122223542, 
+                                     0.391851763867443, 0.0609336025656254, 0.0163914954270103, 0.00225680009502316, 
+                                     0.00047511580947856), 
+                          Percentage = c(52.809122223542, 39.1851763867443, 
+                                     6.09336025656254, 1.63914954270103, 0.225680009502316, 0.047511580947856),
+                          Proportion_name = c("AMACR/Total", "OTHER/Total", "CD3,CD4/Total", "CD3,CD8/Total", "CD3/Total", "PDL-1/Total"),
                           stringsAsFactors = FALSE)
     
     p_cells <- calculate_cell_proportions(sce_object = formatted_image)
@@ -34,7 +38,7 @@ test_that("average_marker_intensity_within_radius() works", {
                                                    target_marker = "CD3",
                                                    radius=30)
     
-    expect_equal(ave_marker_intensity, 3.513, tolerance=1e-3)
+    expect_equal(ave_marker_intensity, 3.717, tolerance=1e-3)
     
 })
 
@@ -43,18 +47,18 @@ test_that("average_minimum_distance() works", {
     
     ave_min_dist <- average_minimum_distance(formatted_image)
     
-    expect_equal(ave_min_dist, 16.990, tolerance=1e-3)
+    expect_equal(ave_min_dist, 15.924, tolerance=1e-3)
     
 })
 
 
 test_that("calculate_summary_distances_between_phenotypes() works", {
     
-    res <- data.frame(Target = c(rep("AMACR", 3)), 
-                      Nearest = c("CD3,CD4", "CD3,CD8", "PDL-1"),
-                      Mean = c(155.5926, 243.5603, 732.2836), 
-                      Std.Dev = c(89.34635, 131.20464, 346.66617),
-                      Median = c(139.8624, 222.6488, 686.4146))
+    res <- data.frame(Target = c("OTHER", "OTHER", "OTHER"),
+                      Nearest = c("AMACR", "CD3,CD4", "CD3,CD8"),
+                      Mean = c(130.418076521445, 70.5496420825272, 134.633340256936), 
+                      Std.Dev = c(123.531481047835, 61.6136499595389, 124.074239935635),
+                      Median = c(85.8661749468322, 52.3545604508337, 97.0463806640928))
     
     summary_distances <- calculate_summary_distances_between_phenotypes(formatted_image)
     rownames(summary_distances) <- NULL
@@ -115,7 +119,8 @@ test_that("identify_cell_clusters() works", {
 
 test_that("identify_cell_communities() works", {
     
-    res <- data.frame(Cell.ID = c("Cell_2", "Cell_3", "Cell_4", "Cell_5"), 
+    res <- data.frame(row.names = 2:5,
+                      Cell.ID = c("Cell_2", "Cell_3", "Cell_4", "Cell_5"), 
                       Phenotype = c("AMACR", "AMACR", "AMACR", "AMACR"), 
                       Cell.X.Position = c(171L, 184L, 201L, 219L), 
                       Cell.Y.Position = c(22L, 38L, 52L, 63L), 
@@ -142,10 +147,10 @@ test_that("identify_cell_communities() works", {
 test_that("marker_permutation() works", {
     
     res <- data.frame(row.names = c("CD3", "PDL-1", "CD4", "CD8"),
-                      Percentage_of_occurrence = c(100, 38, 100, 100),
-                      Observed_cell_number = c(0, 4, 0, 0),
-                      Average_bootstrap_cell_number = c(72.35, 0.45, 55.3, 13.61),
-                      Enrichment.p = c(1, 0.01, 1, 1), 
+                      Percentage_of_occurrence = c(100, 90, 100, 100),
+                      Observed_cell_number = c(19, 4, 0, 0),
+                      Average_bootstrap_cell_number = c(291.78, 1.75, 216.96, 54.72),
+                      Enrichment.p = c(1, 0.06, 1, 1), 
                       Depletion.p = c(0.01, 1, 0.01, 0.01))
     
     sig <- marker_permutation(formatted_image, num_iter = 100)
@@ -158,7 +163,7 @@ test_that("marker_permutation() works", {
 
 test_that("percentage_of_cells_within_radius() works", {
     
-    res <- setNames(c(33.33333, 94.73684, 0.00000, 0.00000 ), c("Cell_664","Cell_2760", "Cell_2992", "Cell_3147"))
+    res <- setNames(c(5.357143, 36.734694, 0.00000, 0.00000 ), c("Cell_664","Cell_2760", "Cell_2992", "Cell_3147"))
     
     percent_cells <- percentage_of_cells_within_radius(formatted_image, reference_phenotypes = "PDL-1", target_phenotypes = "AMACR", radius=100)
     
