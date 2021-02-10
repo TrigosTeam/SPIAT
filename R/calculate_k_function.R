408b4e213c8df1bd241f89d2e41c4a1b1ba31bf6
cancer-evolution <anna.trigos@petermac.org>
2021-02-10 14:00:55 +1100

    Update calculate_k_function.R
    Updated to utilise proper point pattern subsetting
    
    Former-commit-id: 698a48da895e2d7efda14cac15e221e58e01ec31

--- R/calculate_k_function.R
+++ R/calculate_k_function.R
@@ -2,31 +2,35 @@
 #' 
 #' @description Returns K function of cell distribution (inhomogenous distributions)
 #' 
-#' @param sce_object SingleCellExperiment object used as input data for K function 
+#' @param point_pattern ppp object used as input data for K function 
 #' @param determine_confidence indicate the confidence interval of the K function calculated
-#' 
+#' @param cell_phenotype_of_interest string supplying the phenotype of interest
 
-calculate_k_function <- function(sce_object, cell_phenotypes_of_interest = NULL, determine_confidence = NULL, plot_envelope = NULL){
-  #import spatstat package
-  library(spatstat)
+calculate_k_function <- function(point_pattern, cell_phenotype_of_interest = NULL, determine_confidence = NULL, plot_envelope = NULL){
+ library(stringr)
   
-  #create point pattern object
-  point_pattern <- ppp(colData(sce_object)[,2], colData(sce_object)[,3], c(min(colData(sce_object)[,2]), max(colData(sce_object)[,2])), c(min(colData(sce_object)[,3]), max(colData(sce_object)[,3])), marks = as.factor(colData(sce_object)[,1]))
-  
-  if(!is.null(cell_phenotypes_of_interest)) {
-    subset_of_interest <- subset(point_pattern, marks == cell_phenotypes_of_interest)
+  if(!is.null(cell_phenotype_of_interest)) {
+    subset <- split(point_pattern)
+    if(str_detect(cell_phenotype_of_interest, ",")){
+      subset_name <- paste("subset$`", cell_phenotype_of_interest, "`", sep = "")
+    }
+    else{
+      subset_name <- paste("subset$", cell_phenotype_of_interest, sep = "")
+    }
+    subset_of_interest <- eval(parse(text = subset_name))
     point_pattern <- subset_of_interest
   } 
-  k_function <- Kinhom(point_pattern)
+  D <- density(point_pattern)
+  k_function <- Kinhom(point_pattern, correction = "border", D)
   
-
   
   if (!is.null(determine_confidence)) {
     confidence <- lohboot(point_pattern, "Kinhom")
     plot(confidence)
   }
   if (!is.null(plot_envelope)) {
-    envelope_k <- envelope(point_pattern, Kinhom, nsim= 49)
+    envelope_k <- envelope(point_pattern, Kinhom, funargs = c(D, correction = "border"),  simulate = expression(rpoispp(density(subset2))), nsim= 49)
+   
     plot(envelope_k, main = "K-function")
   } 
   else {
