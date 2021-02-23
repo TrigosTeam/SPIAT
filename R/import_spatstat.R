aeccd464bca988c50a0406c711d49ec197b0d396
cancer-evolution <anna.trigos@petermac.org>
2021-02-23 14:43:01 +1100

    Update import_spatstat.R
    Modified to only import libraries required
    
    Former-commit-id: 78aa49396a72618c917582b02c067cb72f446d7a

--- R/import_spatstat.R
+++ R/import_spatstat.R
@@ -1,16 +1,12 @@
-#' Import spatstat library and create multitype point pattern based on 
-#' single cell experiment object
+#' Import spatstat library and stringr library
 #' 
-#' input - single cell experiment object from SPIAT
-#' output - multitype point pattern representative of sce_object
+#' Note: function to be called before calling any spatstat related functions
 #' 
+#' No inputs or outputs
 
-import_spatstat <- function(sce_object) {
+import_spatstat <- function() {
   #import spatstat package
   library(spatstat)
-  
-  #create point pattern object
-  point_pattern <- ppp(colData(sce_object)[,2], colData(sce_object)[,3], c(min(colData(sce_object)[,2]), max(colData(sce_object)[,2])), c(min(colData(sce_object)[,3]), max(colData(sce_object)[,3])), marks = as.factor(colData(sce_object)[,1]))
-  return(point_pattern)
+  library(stringr)
 }
 
