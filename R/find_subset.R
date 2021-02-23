29bbab5611c4cdb63f0749f8d3542821d4a3b23e
cancer-evolution <anna.trigos@petermac.org>
2021-02-23 14:30:54 +1100

    Add files via upload
    Former-commit-id: 54742580872945d320408dd44cde38e03d9823a4

---
+++ R/find_subset.R
@@ -0,0 +1,47 @@
+#' Determine point subset data 
+#' 
+#' find_subset returns unmarked point pattern representing the phenotype/phenotypes 
+#' of interest from the initial image
+#' 
+#' Inputs - 
+#' point_pattern: Marked point pattern representing data
+#' cell_phenotypes_of_interest: String representation of the names of the phenotypes of interest
+#' 
+#' Outputs - 
+#' subset_of_interest: unmarked ppp object which is subset of points representing the phenotype specified, 
+#' 
+#' @param point_pattern ppp object representing intial image
+#' @param cell_phenotypes_of_interest Phenotype/Phenotypes of interest
+#' 
+find_subset <- function(point_pattern, cell_phenotypes_of_interest){
+  # Split initial ppp object into subsets based on marks
+  subset <- split(point_pattern)
+  
+  # Check if there are multiple phenotypes of interest
+  if(length(cell_phenotypes_of_interest) == 1)
+  {
+    # Check whether the phenotype has a comma to indicate multiple markers and act accordingly
+    if(str_detect(cell_phenotypes_of_interest, ",")){
+      subset_name <- paste("subset$`", cell_phenotypes_of_interest, "`", sep = "")
+    }
+    else{
+      subset_name <- paste("subset$", cell_phenotypes_of_interest, sep = "")
+    }
+    subset_of_interest <- eval(parse(text = subset_name))
+  }
+  # Check for multiple phenotypes of interest and act accordingly to superimpose the subsets required
+  else if(length(cell_phenotypes_of_interest) > 1)
+  {
+    for (j in 1:length(cell_phenotypes_of_interest))
+    {
+      if(str_detect(cell_phenotypes_of_interest[j], ",")){
+        subset_name <- paste("subset$`", cell_phenotypes_of_interest[j], "`", sep = "")
+      }
+      else{
+        subset_name <- paste("subset$", cell_phenotypes_of_interest[j], sep = "")
+      }
+      subset_of_interest <- superimpose(subset_of_interest, eval(parse(text = subset_name)))
+    }
+  }
+  return(subset_of_interest)
+}
