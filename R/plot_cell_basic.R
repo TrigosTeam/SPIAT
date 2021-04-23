1ed6120d3676ca015666f8fa2b70497c49092d4a
fuerzhou <653540853@qq.com>
2021-04-23 22:53:00 +1000

    jojo 23rd Apr update
    Former-commit-id: 3a6ff1fecd289b51e684b5606918a68950f5da7e
    Former-commit-id: 6e14507e750f3c16e1a131cf4d6966672ecd2a62
    Former-commit-id: 29b02222dda907172f218ef8ab1e3be47c7e0cf7

--- R/plot_cell_basic.R
+++ R/plot_cell_basic.R
@@ -1,59 +1,75 @@
 #' plot_cell_basic
 #' 
 #' @description Produces a scatter plot of the cells in the tissue. Cells are coloured
-#' categorically by Ceell.Type. Cells not part of the celltypes of interest will be coloured "lightgrey"
+#' categorically by specified column. Cells not part of the celltypes of interest will be coloured "lightgrey"
 #' 
 #' @param sce_object Singlecellexperiment object in the form of the output of format_image_to_sce
-#' @param celltypes_of_interest Vector of cell cell types to be coloured
+#' @param types_of_interest Vector of cell cell types to be coloured
 #' @param colour_vector Vector specifying the colours of each cell phenotype
 #' @param column String specifying the column to be coloured
 #' @import dplyr
 #' @importFrom SummarizedExperiment colData assay
 #' @return A plot is returned
 #' @examples
-#' celltypes_of_interest <- c("AMACR", "CD3,CD8", "PDL-1")
+#' types_of_interest <- c("AMACR", "CD3,CD8", "PDL-1")
 #' colour_vector <- c("darkgrey", "blue", "red")
-#' plot_cell_basic(SPIAT::formatted_image, celltypes_of_interest, colour_vector, column)
+#' plot_cell_basic(SPIAT::formatted_image, types_of_interest, colour_vector, column)
 #' @export
 
-plot_cell_basic <- function (sce_object, celltypes_of_interest, colour_vector, column) 
-{
+plot_cell_basic <- function (sce_object, types_of_interest, colour_vector, column) {
+  
+  
   Cell.X.Position <- Cell.Y.Position <- NULL
   assign(column, NULL)
   formatted_data <- data.frame(colData(sce_object))
 
-  
-  if (length(celltypes_of_interest) != length(colour_vector)) {
+  if (length(types_of_interest) != length(colour_vector)) {
     stop("The colour vector is not the same length as the celltypes of interest")
   }
   
-  for (phenotype in celltypes_of_interest) {
+  real_celltypes <- types_of_interest
+  for (phenotype in types_of_interest) {
     if (!(phenotype %in% unique(formatted_data[[column]]))) {
-      stop(paste(phenotype, "cells were not found"), sep = "")
+      print(paste(phenotype, "cells were not found"), sep = "")
+      real_celltypes <- real_celltypes[real_celltypes != phenotype]
     }
   }
-  if (any(!formatted_data[[column]] %in% celltypes_of_interest)) {
-    formatted_data[!formatted_data[[column]] %in% celltypes_of_interest, 
+  
+  colour_vector <- colour_vector[match(real_celltypes, types_of_interest)]
+  
+  types_of_interest <- real_celltypes
+  
+  if (any(!formatted_data[[column]] %in% types_of_interest)) {
+    formatted_data[!formatted_data[[column]] %in% types_of_interest, 
     ][[column]] <- "OTHER"
   }
+  
   formatted_data$color <- ""
-  for (phenotype in celltypes_of_interest) {
-    idx <- which(celltypes_of_interest == phenotype)
+  
+  for (phenotype in types_of_interest) {
+    idx <- which(types_of_interest == phenotype)
     formatted_data[formatted_data[[column]] == phenotype, 
     ]$color <- colour_vector[idx]
   }
+  
   if (any(formatted_data[[column]] == "OTHER")) {
     formatted_data[formatted_data[[column]] == "OTHER", ]$color <- "grey"
-    all_phenotypes <- c(celltypes_of_interest, "OTHER")
+    all_phenotypes <- c(types_of_interest, "OTHER")
     all_colours <- c(colour_vector, "grey")
   }
+  
   else {
-    all_phenotypes <- celltypes_of_interest
+    all_phenotypes <- types_of_interest
     all_colours <- colour_vector
   }
+
+  name_of_object <- attr(sce_object, "name")
   
   plot(formatted_data$Cell.X.Position,formatted_data$Cell.Y.Position,
-            pch = 19,cex = 0.2, col = formatted_data$color)
+       pch = 19,cex = 0.4, col = formatted_data$color, 
+       xlab = "X Position", ylab = "Y Position",
+       main = paste("Plot", name_of_object, "by" ,column))
+  
   x.max <- max(sce_object$Cell.X.Position)
   y.max <- max(sce_object$Cell.Y.Position)
   x <- x.max
