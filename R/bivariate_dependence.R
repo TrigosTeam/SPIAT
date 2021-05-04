ef12c39ca939852e8c56183a8073d859fdf0aaba
fuerzhou <653540853@qq.com>
2021-05-04 10:48:23 +1000

    spatstat to spatstat.core

--- R/bivariate_dependence.R
+++ R/bivariate_dependence.R
@@ -7,7 +7,7 @@
 #' @param phenotypes Vector of phenotypes of interest
 #' @param column String that is the name of the column of the types
 #' @import SingleCellExperiment
-#' @importFrom spatstat ppp Gcross Kcross Lcross Jcross
+#' @importFrom spatstat.core ppp Gcross Kcross Lcross Jcross
 #' @export 
 
 # colData() is in package 'SummarizedExperiment' but imported by SingleCellExperiment
