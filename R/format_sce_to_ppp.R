e2cd240046c6323e30dcdacc4db19c34597360d6
fuerzhou <653540853@qq.com>
2021-05-04 11:59:27 +1000

    spatstat to spatstat.geom
    Former-commit-id: bdc88bf5170b6533156a985cf07892d2ffe6b7cf
    Former-commit-id: 62f77ac1174ede74fa13dee4418c39e79f8b0135
    Former-commit-id: 7fa1f3a2c7bd173852267ba0560ae91d716618f7

--- R/format_sce_to_ppp.R
+++ R/format_sce_to_ppp.R
@@ -6,7 +6,7 @@
 #'
 #' @export
 #' @param sce_object SingleCellExperiment object in the form of the output of format_image_to_sce
-#' @importFrom spatstat ppp 
+#' @importFrom spatstat.geom ppp 
 #' @importFrom grDevices chull
 
 format_sce_to_ppp <- function(sce_object) {
