976b307f64b0d8eebdd4503eac7dc6a62211b28b
fuerzhou <653540853@qq.com>
2021-05-11 14:41:52 +1000

    upload new functions

---
+++ R/gradient.R
@@ -0,0 +1,22 @@
+#' gradient
+#'
+#' @description Calculate the metrics for all of the specified radii	
+#' @param sce_object SingleCellExperiment object in the form of the output of format_image_to_sce
+#' @param radii Vector specifying the range of radii for the metrics to be calculated
+#' @param FUN Variable name specifying the metric
+#' @param ... Arguments of FUN 
+#' @importFrom SummarizedExperiment colData 
+#' @return A list of the metrics under all radii
+#' @export
+#' 
+gradient <- function(sce_object, radii, FUN, ...){
+    
+  list.metric <- list() 
+  for (i in 1:length(radii)){
+  
+    # mixing_score
+    metric <- FUN(sce_object,radius = radii[i], ...)
+    list.metric[[i]] <- metric 
+  }
+  return(list.metric)
+}
