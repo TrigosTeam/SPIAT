0bcdb5a1294e3c57ffb31af1d82a2c38535672a4
fuerzhou <653540853@qq.com>
2021-05-11 14:41:52 +1000

    upload new functions
    Former-commit-id: 0e8d5c49160f5953f6831df4613ba3f1458b45a9 [formerly 976b307f64b0d8eebdd4503eac7dc6a62211b28b]
    Former-commit-id: abaf9b037d122ede4ee83da985ae9066652e8494
    Former-commit-id: 86a0ec122ee7bff197da97ea9008d2f82cb3781e
    Former-commit-id: 3d0db8247648a0554485b639e97df849edd5b999

---
+++ R/grid_metrics.R
@@ -0,0 +1,33 @@
+#' grid_metrics
+#'
+#' @description Calculate a specified metric for each little grid in the image and plot the metrics for the grids 
+#' @param sce_object SingleCellExperiment object in the form of the output of format_image_to_sce
+#' @param n_spliti Integer specifying the number of splits for the calculation of metrics
+#' @param FUN Variable name specifying the metric to be calculated
+#' @param ... arguments of FUN 
+#' @importFrom SummarizedExperiment colData 
+#' @importFrom raster raster
+#' @return a list of the metrics of all grids
+#' @export
+
+grid_metrics <- function(sce_object, FUN, n_split, ...){
+  split <- image_splitter(sce_object,n_split)
+  list.metric <- list()
+  for (i in 1:length(split)){
+    try <- try(sce <- format_colData_to_sce(split[[i]]))
+    if (class(try) == "SingleCellExperiment"){
+      metric <- FUN(sce, ...)
+      list.metric[[i]] <- metric
+    }
+    else{
+      list.metric[[i]] <- 0.0
+    }
+    
+  }
+  x <- raster(ncol=n_split, nrow=n_split, xmn=0, xmx=max(sce_object$Cell.X.Position), ymn=0, 
+              ymx=max(sce_object$Cell.Y.Position))
+  values(x) <- unlist(list.metric)
+  plot(x)
+  return(list.metric)
+}
+
