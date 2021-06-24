2b154dbd416e374322bf0b65234da10a8eb77bfa
fuerzhou <653540853@qq.com>
2021-06-25 01:09:56 +1000

    change cell names
    Former-commit-id: 1ca8773f85be87b6962c68b2c2e1bacda69ce11e

--- R/calculate_proportions_of_cells_in_structure.R
+++ R/calculate_proportions_of_cells_in_structure.R
@@ -10,22 +10,23 @@ calculate_proportions_of_cells_in_structure <- function(sce_object){
   data <- data.frame(colData(sce_object))
   
   # proportion of infiltrated immune cells that are in the tumour region
-  p_i_in <- length(which(data$Structure == "Infiltration"))/length(which(data$Region == "In"))
+  p_i_in <- length(which(data$Structure == "Infiltrated.immune"))/length(which(data$Region == "Inside"))
   # proportion of immune cells that are in the tumour side of the invasive front
-  p_i_i.f.in <- length(which(data$Structure == "I.f.immune.in"))/
-    length(which(data$Structure == "Invasive.front.in"))
+  p_i_i.f.in <- length(which(data$Structure == "Internal.margin.immune"))/
+    length(which(data$Structure == "Internal.margin"))
   # proportion of immune cells of the invasive front that are outside of the tumour 
-  p_i_i.f.out <- length(which(data$Structure == "I.f.immune.out"))/
-    length(which(data$Structure == "Invasive.front.out"))
+  p_i_i.f.out <- length(which(data$Structure == "External.margin.immune"))/
+    length(which(data$Structure == "External.margin"))
   # proportion of exclusice immune cells that are out of the tumour region
-  p_i_out <- length(which(data$Structure == "Exclusion"))/length(which(data$Region == "Out"))
+  p_i_out <- length(which(data$Structure == "Stromal.immune"))/length(which(data$Region == "Outside"))
   
   #save in dataframe
-  Infiltration <- p_i_in
-  Exclusion <- p_i_out
-  Invasive.front.in <- p_i_i.f.in
-  Invasive.front.out <- p_i_i.f.out
-  proportion.data <- data.frame(Infiltration, Exclusion, Invasive.front.in, Invasive.front.out)
+  Infiltrated.immune <- p_i_in
+  Stromal.immune <- p_i_out
+  Internal.margin.immune <- p_i_i.f.in
+  External.margin.immune <- p_i_i.f.out
+  proportion.data <- data.frame(Infiltrated.immune, Stromal.immune, 
+                                Internal.margin.immune, External.margin.immune)
   
   return(proportion.data)
 }
