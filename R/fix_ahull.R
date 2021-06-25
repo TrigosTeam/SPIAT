43be0d0dffe4dfc690537acdf8f1857adec712a4
fuerzhou <653540853@qq.com>
2021-06-25 12:12:00 +1000

    function for fixing ahull
    Former-commit-id: 8a0ebef5a6890a7ba46df40b25f893db1255ba7e [formerly 625962619693a7458565fb17263224a6b2c72f80]
    Former-commit-id: 230aa8d36dd1c04fe6cf19a741ffd81e30531efe
    Former-commit-id: f89aca77166be3b9ed2ff851bc39fe971c22145c
    Former-commit-id: b6d3301c88fff808f594d993d16ee9a3f0c11dde

---
+++ R/fix_ahull.R
@@ -0,0 +1,67 @@
+fix_ahull <- function(ahull){ # the order of cells returned by ahull is messy
+                              # this function reorders the cells
+  arc <- ahull$arcs
+  n_cells <- dim(arc)[1]
+  # copy the arc ends
+  ends <- arc
+  ends <- cbind(ends, 1:n_cells)
+  ends <- rbind(ends, ends[1,])
+  i <- 1
+  while (i < n_cells){
+    end2 <- ends[i,8]
+    next_end1 <- ends[i+1,7]
+    next_end2 <- ends[i+1,8]
+    print(end2)
+    print(next_end1)
+    while (end2 != next_end1){ # the connection breaks here
+      if (next_end2 == next_end1) {
+        # if the next cell is a loner cell, or the current cell is the last cell,
+        # the current cell does not have to be issue cell
+        delete_ID <- c(delete_ID, i+1) # store the loner cell
+        i <- i+1
+        if (i >= (dim(arc)[1])){
+          break
+        }
+        next_end1 <- arc[i+1,7]
+        next_end2 <- arc[i+1,8]
+      }
+      
+      else{ # the next cell is not loner cell, confirm the current cell is the issue cell
+        # break the current loop, go to next cell (skip loner cells)
+        issue_ID <- c(issue_ID, i+1) 
+        for (j in (i+1):n_cells){
+          t_end1 <- ends[j,7]
+          t_end2 <- ends[j,8]
+          # found the correct cell!
+          if (t_end1 == end2 || t_end2 == end2){ 
+            ends[n_cells+1,] <- ends[i+1,]
+            ends[i+1,] <- ends[j,]
+            ends[j,] <- ends[n_cells+1,]
+          }
+          if (t_end2 == end2){
+            ends[i+1,8] <- t_end1
+            ends[i+1,7] <- end2
+          }
+        }
+        break
+      }
+    }
+    # next cell
+    i <- i+1
+  }
+  ends <- ends[-(n_cells+1),]
+  ends <- ends[,-9]
+  View(ends)
+  ahull$arcs <- ends
+  return(ahull)
+}
+
+get_polygon <- function(xahull, arc){ # this function gets the coordinates that on ahull
+  cell_ID = c()
+  locs <- c()
+  for (i in 1: (dim(arc)[1])){
+    cell_ID = arc[i,7]
+    locs <- rbind(locs,xahull[cell_ID,c(1,2)])
+  }
+  return(locs)
+}
