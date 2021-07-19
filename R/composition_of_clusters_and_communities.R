82d976a67de45f0b87de229a117d499c89e28589
fuerzhou <653540853@qq.com>
2021-07-19 10:38:16 +1000

    add param `column` & `type_of_aggregate`

--- R/composition_of_clusters_and_communities.R
+++ R/composition_of_clusters_and_communities.R
@@ -3,7 +3,8 @@
 #' @description Returns a dataframe which contains the percentages of cells with a
 #' specific marker within each cluster and the number of cells in the cluster.
 #' @param formatted_data_with_clusters - a dataframe output from generate_clusters
-#' @param column_to_consider Column name to consider as community/clusters
+#' @param type_of_aggregate Cluster or Community
+#' @param column Column with cell types
 #' @importFrom stats aggregate
 #' @return A data.frame is returned
 #' @examples
@@ -11,17 +12,19 @@
 #' communities_vis <- composition_of_clusters_and_communities(communities, "Community")
 #' @export
 
-composition_of_clusters_and_communities <- function(formatted_data_with_clusters, column_to_consider) {
-    number_of_clusters <- length(unique(formatted_data_with_clusters[,column_to_consider]))
+composition_of_clusters_and_communities <- function(formatted_data_with_clusters, type_of_aggregate, column) {
+    number_of_clusters <- length(unique(formatted_data_with_clusters[,type_of_aggregate]))
 
-    if(column_to_consider == "Community"){
-      composition <- aggregate(Cell.ID ~ Phenotype + Community, formatted_data_with_clusters, length)
+    colnames(formatted_data_with_clusters)[colnames(formatted_data_with_clusters) == column] <- "Temp_pheno"
+    
+    if(type_of_aggregate == "Community"){
+      composition <- aggregate(Cell.ID ~ Temp_pheno + Community, formatted_data_with_clusters, length)
       colnames(composition)[3] <- "Number_of_cells"
       cluster_size <- table(formatted_data_with_clusters$Community)
       composition$Total_number_of_cells <- as.vector(cluster_size[match(composition$Community, names(cluster_size))])
 
-    }else if(column_to_consider == "Cluster"){
-      composition <- aggregate(Cell.ID ~ Phenotype + Cluster, formatted_data_with_clusters, length)
+    }else if(type_of_aggregate == "Cluster"){
+      composition <- aggregate(Cell.ID ~ Temp_pheno + Cluster, formatted_data_with_clusters, length)
       colnames(composition)[3] <- "Number_of_cells"
       cluster_size <- table(formatted_data_with_clusters$Cluster)
       composition$Total_number_of_cells <- as.vector(cluster_size[match(composition$Cluster, names(cluster_size))])
