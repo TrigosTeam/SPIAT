77d6e1b8ed5edaef48a8ad0336fb892009940fbe
fuerzhou <653540853@qq.com>
2021-07-19 10:14:29 +1000

    account for no data

--- R/average_nearest_neighbor_index.R
+++ R/average_nearest_neighbor_index.R
@@ -19,31 +19,37 @@ average_nearest_neighbor_index <- function(sce_object, reference_cell, column){
   
   data <- data.frame(colData(sce_object))[,c(column ,"Cell.X.Position","Cell.Y.Position")]
   data <- data[which(data[,column] == reference_cell),c("Cell.X.Position","Cell.Y.Position") ]
-  object<-format_colData_to_sce(data)
   
-  ppp_object <- format_sce_to_ppp(object)
-  ann.p <- mean(nndist(ppp_object, k=1))
-  
-  n <- ppp_object$n      # Number of points
-  
-  x <- ppp$window$xrange[2] - ppp$window$xrange[1]
-  y <- ppp$window$yrange[2] - ppp$window$yrange[1]
-  area <- x*y
-  ann.e <- 0.5/sqrt(n/area)
-  se <- 0.26136/sqrt(n*n/area)
-  z <- (ann.p - ann.e)/se
-  p <- pnorm(-abs(z))
-  
-  if (p <= 5e-3){
-    if (ann.p <= ann.e){
-      pattern <- "Clustered"
+  if(nrow(data) == 0){
+    print("No reference cells found")
+    ANN_index <- list(pattern=NA,`p-value`=NA)
+  }else{
+    object<-format_colData_to_sce(data)
+    
+    ppp_object <- format_sce_to_ppp(object)
+    ann.p <- mean(nndist(ppp_object, k=1))
+    
+    n <- ppp_object$n      # Number of points
+    
+    x <- ppp$window$xrange[2] - ppp$window$xrange[1]
+    y <- ppp$window$yrange[2] - ppp$window$yrange[1]
+    area <- x*y
+    ann.e <- 0.5/sqrt(n/area)
+    se <- 0.26136/sqrt(n*n/area)
+    z <- (ann.p - ann.e)/se
+    p <- pnorm(-abs(z))
+    
+    if (p <= 5e-6){
+      if (ann.p <= ann.e){
+        pattern <- "Clustered"
+      }
+      else{pattern <- "Dispersed"}
     }
-    else{pattern <- "Dispersed"}
+    else{pattern <- "Random"}
+    
+    ANN_index <- list(pattern, p)
+    names(ANN_index) <- c("pattern" ,"p-value")
   }
-  else{pattern <- "Random"}
-  
-  ANN_index <- list(pattern, p)
-  names(ANN_index) <- c("pattern" ,"p-value")
   
   return(ANN_index)
 }
