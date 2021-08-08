1f79fe7148bcbcc9ba783f0254c14e3d9ce96932
fuerzhou <653540853@qq.com>
2021-08-09 07:56:36 +1000

    better define the param `n_margin_layers` & print more information
    Former-commit-id: 09087e7ea215feab7fbd57b0babc030842f21bd6 [formerly ecbfa27c9b1586a0a76efc87f05406923ef98771]
    Former-commit-id: 544a00e5d61b3c8ae424bbc22d121ccabbfffe90
    Former-commit-id: 710fb5eaaf7a9efcc5db47626c77eef8fbbb59b1
    Former-commit-id: cac1028cf07d1ae5d230a453188909a6c8e17dc4
    Former-commit-id: 2385a3695e69e1cf41eec90bc0699228673006ba

--- R/define_structure.R
+++ R/define_structure.R
@@ -3,8 +3,8 @@
 #' @description Identify the cells that compose the invasive front, the infiltration and the exclusion
 #'
 #' @param sce_object SingleCellExperiment object in the form of the output of format_image_to_sce
-#' @param names_of_immune_cells vector indicating the names of potential immune cells
-#' @param n_invasive Number specifying the number of cells that are included in the invasive front
+#' @param names_of_immune_cells Vector indicating the names of potential immune cells
+#' @param n_margin_layers Integer Specifying the number of layers of cells that compose the internal/external margins
 #' @import SingleCellExperiment
 #' @import dplyr
 #' @export
@@ -14,8 +14,9 @@ define_structure <- function(sce_object, names_of_immune_cells, n_invasive = 5){
   
   # find the distance of invasive front
   min_dist <- average_minimum_distance(sce_object)
-  invasive_dist <- n_invasive * min_dist
-  print(invasive_dist)
+  margin_dist <- n_margin_layers * min_dist
+  print(paste("How many layers of cells in the external/internal margin:", n_margin_layers))
+  print(paste("The width of internal/external margin:", margin_dist))
   
   #CHECK if the distance to bordering cells is calculated
   if (is.null(sce_object$Distance.To.Border)){
