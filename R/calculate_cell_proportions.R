ac4f3dd3734257d5fa678485b3f186048336fc90
Maria Doyle <maria.doyle@petermac.org>
2021-09-28 14:45:18 +0100

    Add updated functions from Anna
    Former-commit-id: 71893d2029a87bc6faf6127b1cf1ac3c7b4e00aa [formerly 8453f36ba76c792d9b5e9d75e2698156b803e439]
    Former-commit-id: f4fc524d37c87ec19f4f911a8f1e19e05ad78337
    Former-commit-id: b7261cfb64dd1e32959e1e065d2d008880ac02f2
    Former-commit-id: d3b07e12d33a9ecc09838a7805dc52079d55ae3b
    Former-commit-id: d73791242d0c7369e9ebb6cc40350fa2eceba379

--- R/calculate_cell_proportions.R
+++ R/calculate_cell_proportions.R
@@ -1,19 +1,18 @@
 #' calculate_cell_proportions
 #'
-#' @description Calculate the number and proportion of each cell phenotype in image
+#' @description Calculate the number and proportion of each cell phenotype in an image
 #' @param sce_object SingleCellExperiment object in the form of the output of format_image_to_sce
-#' @param reference_celltypes Vector specifying reference phenotypes. For example, "CD3" will calculate the proportion
-#' of each CD3-containing phenotype against all CD3. "Total" can be used to calculate the proportion of each phenotype against total cells and is the default.
-#' @param celltypes_to_exclude Vector specifying celltypes to exclude. For example "OTHER" will exclude that celltype from the Total (optional)
-#' @param column Column of cells to choose the phenotype from (e.g. Cell.Type, Cell.Type2, etc)
+#' @param reference_celltypes Vector specifying reference phenotypes. If NULL (default), then the proportion of each cell type against all cells is returned.
+#' Alternatively, a custom vector of cell types can be used as input, and these will be used as the denominator in the calculation of the proportions.
+#' @param celltypes_to_exclude Vector specifying cell types to exclude. For example "OTHER" will exclude that celltype from the Total. If NULL, all cell types are included
+#' @param column Column of cells to choose the phenotype from (e.g. Phenotype, Cell.Type, etc)
 #' @importFrom SummarizedExperiment colData
 #' @importFrom stats complete.cases
 #' @return A data.frame is returned
 #' @examples
-#' p_cells <- calculate_cell_proportions(SPIAT::formatted_image, reference_celltypes=c("Total", "CD3"))
 #' @export
 
-calculate_cell_proportions <- function(sce_object, column="Phenotype", reference_celltypes = "Total", celltypes_to_exclude = NULL){
+calculate_cell_proportions <- function(sce_object, column="Phenotype", reference_celltypes = NULL, celltypes_to_exclude = NULL){
 
     #Reads the image file and deletes cell rows with NA positions
     cell_loc <- data.frame(colData(sce_object))
@@ -36,38 +35,27 @@ calculate_cell_proportions <- function(sce_object, column="Phenotype", reference
         }
     } 
     
-    #Calculate proportions for each celltype specified   
-    datalist = list()
+    if(is.null(reference_celltypes)){
+        celltype_cells <- cell_proportions
+        celltype_cells_total <- sum(celltype_cells$Number_of_celltype)
+        celltype_cells$Proportion <- celltype_cells$Number_of_celltype/celltype_cells_total
+        celltype_cells$Percentage <- celltype_cells$Proportion*100
+        celltype_cells$Proportion_name <- paste(celltype_cells$Cell_type, "Total", sep="/")  
+        cell_proportions$Reference <- "Total"
+        cell_proportions <- celltype_cells
     
-    for (celltype in reference_celltypes) {
-        
-        if (celltype != "Total") {
-            
-            celltype_cells <- cell_proportions[grepl(celltype, cell_proportions$Cell_type), ]
-            
-        } else {
-        
-            celltype_cells <- cell_proportions
-            
-        }
-        
-            celltype_cells_total <- sum(celltype_cells$Number_of_celltype)
-            celltype_cells$Reference <- rep(celltype, nrow(celltype_cells))
-            celltype_cells$Number_of_reference <- rep(celltype_cells_total, nrow(celltype_cells))
-            celltype_cells$Proportion <- celltype_cells$Number_of_celltype/celltype_cells_total
-            celltype_cells$Percentage <- celltype_cells$Proportion*100
-            celltype_cells$Proportion_name <- paste(celltype_cells$Cell_type, celltype, sep="/")
-        
-        datalist[[celltype]] <- celltype_cells
-        
+    }else{
+        celltype_cells <- cell_proportions
+        celltype_cells_total <- sum(celltype_cells$Number_of_celltype[celltype_cells$Cell_type %in% reference_celltypes])
+        celltype_cells$Proportion <- celltype_cells$Number_of_celltype/celltype_cells_total
+        celltype_cells$Percentage <- celltype_cells$Proportion*100
+        celltype_cells$Proportion_name <- paste(celltype_cells$Cell_type, "Custom", sep="/")   
+        cell_proportions <- celltype_cells
+        cell_proportions$Reference <- paste(reference_celltypes, collapse=",")
     }
     
-    cell_proportions <- do.call(rbind, datalist)
-    rownames(cell_proportions) <- NULL
-    
     #order by Reference celltype (reverse to have Total first if present) then by highest proportion
-    cell_proportions <- cell_proportions[rev(order(cell_proportions$Reference, cell_proportions$Proportion)), ]
-    
+    cell_proportions <- cell_proportions[rev(order(cell_proportions$Proportion)), ]
     
     return(cell_proportions)
 }
--- R/plot_distance_heatmap.R
+++ R/plot_distance_heatmap.R
@@ -7,18 +7,18 @@
 #' @import ggplot2
 #' @return A plot is returned
 #' @examples
-#' summary_distances <- calculate_summary_distances_between_cell_types(SPIAT::formatted_image)
+#' summary_distances <- calculate_summary_distances_between_phenotypes(SPIAT::formatted_image)
 #' plot_distance_heatmap(summary_distances)
 #' @export
 
 plot_distance_heatmap <- function(phenotype_distances_result, metric = "mean"){
   
     # setting these variables to NULL as otherwise get "no visible binding for global variable" in R check
-    Target <- Nearest <- Mean <- Std.Dev <- Median <- NULL
+    Reference <- Nearest <- Mean <- Std.Dev <- Median <- NULL
 
     if(metric == "mean"){
       limit <- range(unlist(phenotype_distances_result$Mean), na.rm=TRUE)
-      g <- ggplot(phenotype_distances_result, aes(x = Target, y = Nearest, fill = Mean)) +
+      g <- ggplot(phenotype_distances_result, aes(x = Reference, y = Nearest, fill = Mean)) +
         geom_tile() +
         xlab("Cell of interest (COI)") +
         ylab("Nearest cell to COI") +
@@ -29,7 +29,7 @@ plot_distance_heatmap <- function(phenotype_distances_result, metric = "mean"){
 
     }else if(metric == "std.dev"){
       limit <- range(unlist(phenotype_distances_result$Std.Dev), na.rm=TRUE)
-      g <- ggplot(phenotype_distances_result, aes(x = Target, y = Nearest, fill = Std.Dev)) + geom_tile() + xlab("Cell of interest (COI)") + ylab("Nearest cell to COI") +
+      g <- ggplot(phenotype_distances_result, aes(x = Reference, y = Nearest, fill = Std.Dev)) + geom_tile() + xlab("Cell of interest (COI)") + ylab("Nearest cell to COI") +
         theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_rect(fill = "white"),
               axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
         scale_fill_viridis_c(limits = limit, direction = -1)
@@ -37,7 +37,7 @@ plot_distance_heatmap <- function(phenotype_distances_result, metric = "mean"){
 
     }else if(metric == "median"){
       limit <- range(unlist(phenotype_distances_result$Median), na.rm=TRUE)
-      g <- ggplot(phenotype_distances_result, aes(x = Target, y = Nearest, fill = Median)) + geom_tile() + xlab("Cell of interest (COI)") + ylab("Nearest cell to COI") +
+      g <- ggplot(phenotype_distances_result, aes(x = Reference, y = Nearest, fill = Median)) + geom_tile() + xlab("Cell of interest (COI)") + ylab("Nearest cell to COI") +
         theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_rect(fill = "white"),
               axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
         scale_fill_viridis_c(limits = limit, direction = -1)
