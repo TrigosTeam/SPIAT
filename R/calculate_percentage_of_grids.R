088b228a780b2b5a8798afb950f3cd35986391e8
fuerzhou <653540853@qq.com>
2021-12-13 09:30:32 +1100

    add prevalence and distinctiveness functions
    Former-commit-id: 537962d995aa91af75db1cb0e07045c58b55eba8

---
+++ R/calculate_percentage_of_grids.R
@@ -0,0 +1,15 @@
+#' calculate_percentage_of_grids
+#'
+#' @description Calculate the percentage of the grid squares that have a certain pattern
+#'
+#' @param raster_obj Raster object in the form of the output of raster function
+#' @param threshold Number that is the threshold for defining the pattern
+#' @param above Boolean idicating the pattern is above (TRUE) or below (FALSE) the threshold
+#' @export
+
+calculate_percentage_of_grids <- function(raster_obj, threshold){
+  raster_obj@data@values[is.na(raster_obj@data@values)] <- 0
+  if (above) p <- sum(raster_obj@data@values>=threshold)/length(raster_obj@data@values) * 100
+  else p <- sum(raster_obj@data@values<threshold)/length(raster_obj@data@values) * 100
+  return(p)
+}
