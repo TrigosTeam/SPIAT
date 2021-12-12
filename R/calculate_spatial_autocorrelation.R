d50e665ac8059ff9bb226abef36d40f07d2426ab
fuerzhou <653540853@qq.com>
2021-12-13 09:30:32 +1100

    add prevalence and distinctiveness functions
    Former-commit-id: 537962d995aa91af75db1cb0e07045c58b55eba8
    Former-commit-id: 2dfb17431825238ec60e11f54e76ba3634ab9a6b
    Former-commit-id: 5eb4e29b8a3255f43bf9285a3a8051405d4bb924
    Former-commit-id: 0894e7c8aa3e319277658f0b6716ccccd92c6d72

---
+++ R/calculate_spatial_autocorrelation.R
@@ -0,0 +1,17 @@
+#' caculate_spatial_autocorrelaiton
+#'
+#' @description Calculate the spatial autocorrelation metric of the grid squares that have a certain pattern
+#'
+#' @param raster_obj Raster object in the form of the output of raster function
+#' @param metric String The method for calculating spatial autocorrelation. Choose from "globalmoran" and "GearyC"
+#' @import raster
+#' @importFrom elsa geary moran
+#' @export
+
+caculate_spatial_autocorrelaiton <- function(raster, metric = "globalmoran"){
+  raster@data@values[is.na(raster@data@values)] = 0
+  if (metric == "GearyC"){
+    return(geary(raster, d1=0, d2=600))
+  }
+  return(moran(raster, d1=0, d2=600))
+}
