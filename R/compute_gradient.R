8b87a35f0cc7c542a55699aef1d57ace0b72cac2
fuerzhou <653540853@qq.com>
2021-12-13 15:14:37 +1100

    change file name

--- R/compute_gradient.R
+++ R/compute_gradient.R
@@ -1,19 +1,19 @@
-#' gradient
+#' compute_gradient
 #'
 #' @description Calculate the metrics for all of the specified radii	
 #' @param sce_object SingleCellExperiment object in the form of the output of format_image_to_sce
 #' @param radii Vector specifying the range of radii for the metrics to be calculated
 #' @param FUN Variable name specifying the metric
 #' @param ... Arguments of FUN 
-#' @importFrom SummarizedExperiment colData 
+#' @importFrom SingleCellExperiment colData 
 #' @return A list of the metrics under all radii
 #' @export
 #' 
-gradient <- function(sce_object, radii, FUN, ...){
-    
+compute_gradient <- function(sce_object, radii, FUN, ...){
+  
   list.metric <- list() 
   for (i in 1:length(radii)){
-  
+    
     metric <- FUN(sce_object,radius = radii[i], ...)
     list.metric[[i]] <- metric 
   }
