2a020dd25e3684c15f83b38040e19eeac6e99450
fuerzhou <653540853@qq.com>
2021-12-13 14:33:09 +1100

    param "feature_colname"

--- R/calculate_cell_proportions.R
+++ R/calculate_cell_proportions.R
@@ -5,14 +5,14 @@
 #' @param reference_celltypes Vector specifying reference phenotypes. If NULL (default), then the proportion of each cell type against all cells is returned.
 #' Alternatively, a custom vector of cell types can be used as input, and these will be used as the denominator in the calculation of the proportions.
 #' @param celltypes_to_exclude Vector specifying cell types to exclude. For example "OTHER" will exclude that celltype from the Total. If NULL, all cell types are included
-#' @param column Column of cells to choose the phenotype from (e.g. Phenotype, Cell.Type, etc)
+#' @param feature_colname Column of cells to choose the phenotype from (e.g. Phenotype, Cell.Type, etc)
 #' @importFrom SummarizedExperiment colData
 #' @importFrom stats complete.cases
 #' @return A data.frame is returned
 #' @examples
 #' @export
 
-calculate_cell_proportions <- function(sce_object, column="Phenotype", reference_celltypes = NULL, celltypes_to_exclude = NULL){
+calculate_cell_proportions <- function(sce_object, feature_colname="Phenotype", reference_celltypes = NULL, celltypes_to_exclude = NULL){
 
     #Reads the image file and deletes cell rows with NA positions
     cell_loc <- data.frame(colData(sce_object))
@@ -24,14 +24,14 @@ calculate_cell_proportions <- function(sce_object, column="Phenotype", reference
     }
 
     #Creates frequency/bar plot of all cell types in the entire image
-    cell_proportions <- as.data.frame(table(cell_loc[,column]))
+    cell_proportions <- as.data.frame(table(cell_loc[,feature_colname]))
     names(cell_proportions)[1] <- 'Cell_type'
     names(cell_proportions)[2] <- 'Number_of_celltype'
     
     #Exclude any phenotypes not wanted
     if (!is.null(celltypes_to_exclude)) {
         for (celltype in celltypes_to_exclude) {
-            cell_proportions <- cell_proportions[!grepl(celltype, cell_proportions$Cell_type), ]
+            cell_proportions <- cell_proportions[!grepl(celltype, cell_proportions[[feature_colname]]), ]
         }
     } 
     
@@ -40,16 +40,16 @@ calculate_cell_proportions <- function(sce_object, column="Phenotype", reference
         celltype_cells_total <- sum(celltype_cells$Number_of_celltype)
         celltype_cells$Proportion <- celltype_cells$Number_of_celltype/celltype_cells_total
         celltype_cells$Percentage <- celltype_cells$Proportion*100
-        celltype_cells$Proportion_name <- paste(celltype_cells$Cell_type, "Total", sep="/")  
+        celltype_cells$Proportion_name <- paste(celltype_cells[[feature_colname]], "Total", sep="/")  
         cell_proportions$Reference <- "Total"
         cell_proportions <- celltype_cells
     
     }else{
         celltype_cells <- cell_proportions
-        celltype_cells_total <- sum(celltype_cells$Number_of_celltype[celltype_cells$Cell_type %in% reference_celltypes])
+        celltype_cells_total <- sum(celltype_cells$Number_of_celltype[celltype_cells[[feature_colname]] %in% reference_celltypes])
         celltype_cells$Proportion <- celltype_cells$Number_of_celltype/celltype_cells_total
         celltype_cells$Percentage <- celltype_cells$Proportion*100
-        celltype_cells$Proportion_name <- paste(celltype_cells$Cell_type, "Custom", sep="/")   
+        celltype_cells$Proportion_name <- paste(celltype_cells[[feature_colname]], "Custom", sep="/")   
         cell_proportions <- celltype_cells
         cell_proportions$Reference <- paste(reference_celltypes, collapse=",")
     }
