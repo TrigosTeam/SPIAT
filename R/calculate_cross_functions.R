ad49ae1a663262a2e2e3eee126d19554ec8b3c87
fuerzhou <653540853@qq.com>
2021-12-13 14:38:41 +1100

    change name; call format_sce_to_ppp to format ppp
    Former-commit-id: bed8a88c4ea14e9d900db8ad0e11308a3310021e

--- R/calculate_cross_functions.R
+++ R/calculate_cross_functions.R
@@ -1,59 +1,51 @@
 #' calculate_cross_functions
 #'
-#' @description calculate the cross functions between two cell types to see if there is mixing between them
+#' @description calculate the cross functions between two specified cell types 
 #' 
 #' @param sce_object SingleCellExperiment object in the form of the output of format_image_to_sce
 #' @param method String that is the method for dependence calculation. Options: "Gcross", "Kcross", "Lcross", "Jcross"
 #' @param phenotypes Vector of phenotypes of interest
-#' @param column String that is the name of the column of the types
-#' @import SingleCellExperiment
+#' @param feature_colname String that is the name of the column of the types
+#' @param plot_results TRUE if result to be plotted, FALSE if not. In either case, an object with the results is returned
 #' @importFrom spatstat.core Gcross Kcross Lcross Jcross
 #' @importFrom spatstat.geom ppp 
 #' @export 
 
 calculate_cross_functions <- function(sce_object, method = "Kcross", 
-                                 phenotypes, column) {
-  
-  formatted_data <- colData(sce_object)
+                                      phenotypes, feature_colname, plot_results = T) {
   
   #CHECK
-  if (!all(phenotypes %in% formatted_data[[column]])) {
+  if (!all(phenotypes %in% formatted_data[[feature_colname]])) {
     stop("Cell type not found!")
   }
   
-  # get x, y coordinates and specified cell types from formatted_data
-  x <- formatted_data$Cell.X.Position
-  y <- formatted_data$Cell.Y.Position
-  marks <- formatted_data[[column]]
-  
-  # get windows
-  x_summary <- summary(formatted_data$Cell.X.Position)
-  x_min <- as.numeric(x_summary[1])
-  x_max <- as.numeric(x_summary[6])
-  y_summary <- summary(formatted_data$Cell.Y.Position)
-  y_min <- as.numeric(y_summary[1])
-  y_max <- as.numeric(y_summary[6])
-  
-  
-  # format sce to ppp
-  ppp_object <- ppp(x, y, c(x_min,x_max),c(y_min,y_max), marks = marks)
+  # format sce to ppp object
+  ppp_object <- format_sce_to_ppp(sce_object)
   ppp_object$marks <- as.factor(ppp_object$marks)
   
   if (method == "Gcross"){
     p <- Gcross(ppp_object, phenotypes[1],phenotypes[2],correction = "border")
-    plot(p, main = paste("cross G function",attr(sce_object,"name")))
+    if(plot_results){
+      plot(p, main = paste("cross G function",attr(sce_object,"name")))
+    }
   }
   if (method == "Kcross"){
     p <- Kcross(ppp_object, phenotypes[1],phenotypes[2],correction = "border")
-    plot(p, main = paste("cross K function",attr(sce_object,"name")))
+    if(plot_results){
+      plot(p, main = paste("cross K function",attr(sce_object,"name")))
+    }
   }
   if (method == "Lcross"){
     p <- Lcross(ppp_object, phenotypes[1],phenotypes[2],correction = "border")
-    plot(p, main = paste("cross L function",attr(sce_object,"name")))
+    if(plot_results){
+      plot(p, main = paste("cross L function",attr(sce_object,"name")))
+    }
   }
   if (method == "Jcross"){
     p <- Jcross(ppp_object, phenotypes[1],phenotypes[2],correction = "border")
-    plot(p, main = paste("cross J function",attr(sce_object,"name")))
+    if(plot_results){
+      plot(p, main = paste("cross J function",attr(sce_object,"name")))
+    }
   }
   return(p)
 }
