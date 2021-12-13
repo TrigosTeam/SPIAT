235d6ea8e2626334eee082984348f7ac7c753f27
fuerzhou <653540853@qq.com>
2021-12-13 15:12:25 +1100

    change params names
    Former-commit-id: 7127b759c03cb288d94825000f9987c0ffdac0d0 [formerly d21baddeb71c6a7767a56bdbabb94dd375a09bf8]
    Former-commit-id: 2162cc9ab2ace68c7364c95f2de01b7cc4627a7d
    Former-commit-id: ffbd81f6440876824f3ad7069c58f5e1e9f30408
    Former-commit-id: 53b4ef0a1694cac901eec2a0126934bda563d5b9
    Former-commit-id: 81624a9e467fa2a0e27d92e587890cef1e908d74

--- R/plot_cell_basic.R
+++ R/plot_cell_basic.R
@@ -4,9 +4,9 @@
 #' categorically by specified column. Cells not part of the celltypes of interest will be coloured "lightgrey"
 #' 
 #' @param sce_object Singlecellexperiment object in the form of the output of format_image_to_sce
-#' @param types_of_interest Vector of cell cell types to be coloured
+#' @param cell_types_of_interest Vector of cell cell types to be coloured
 #' @param colour_vector Vector specifying the colours of each cell phenotype
-#' @param column String specifying the column to be coloured
+#' @param feature_colname String specifying the column to be coloured
 #' @param cex Number specifying the size of the points on the plot. Default is 0.4.
 #' @import dplyr
 #' @importFrom SummarizedExperiment colData assay
@@ -14,50 +14,50 @@
 #' @return A plot is returned
 #' @export
 
-plot_cell_basic <- function (sce_object, types_of_interest, colour_vector, column, cex = 0.4) {
+plot_cell_basic <- function (sce_object, cell_types_of_interest, colour_vector, feature_colname, cex = 0.4) {
   
   
   Cell.X.Position <- Cell.Y.Position <- NULL
-  assign(column, NULL)
+  assign(feature_colname, NULL)
   formatted_data <- data.frame(colData(sce_object))
 
-  if (length(types_of_interest) != length(colour_vector)) {
+  if (length(cell_types_of_interest) != length(colour_vector)) {
     stop("The colour vector is not the same length as the celltypes of interest")
   }
   
-  real_celltypes <- types_of_interest
-  for (phenotype in types_of_interest) {
-    if (!(phenotype %in% unique(formatted_data[[column]]))) {
+  real_celltypes <- cell_types_of_interest
+  for (phenotype in cell_types_of_interest) {
+    if (!(phenotype %in% unique(formatted_data[[feature_colname]]))) {
       print(paste(phenotype, "cells were not found"), sep = "")
       real_celltypes <- real_celltypes[real_celltypes != phenotype]
     }
   }
   
-  colour_vector <- colour_vector[match(real_celltypes, types_of_interest)]
+  colour_vector <- colour_vector[match(real_celltypes, cell_types_of_interest)]
   
-  types_of_interest <- real_celltypes
+  cell_types_of_interest <- real_celltypes
   
-  if (any(!formatted_data[[column]] %in% types_of_interest)) {
-    formatted_data[!formatted_data[[column]] %in% types_of_interest, 
-    ][[column]] <- "OTHER"
+  if (any(!formatted_data[[feature_colname]] %in% cell_types_of_interest)) {
+    formatted_data[!formatted_data[[feature_colname]] %in% cell_types_of_interest, 
+    ][[feature_colname]] <- "OTHER"
   }
   
   formatted_data$color <- ""
   
-  for (phenotype in types_of_interest) {
-    idx <- which(types_of_interest == phenotype)
-    formatted_data[formatted_data[[column]] == phenotype, 
+  for (phenotype in cell_types_of_interest) {
+    idx <- which(cell_types_of_interest == phenotype)
+    formatted_data[formatted_data[[feature_colname]] == phenotype, 
     ]$color <- colour_vector[idx]
   }
   
-  if (any(formatted_data[[column]] == "OTHER")) {
-    formatted_data[formatted_data[[column]] == "OTHER", ]$color <- "grey"
-    all_phenotypes <- c(types_of_interest, "OTHER")
+  if (any(formatted_data[[feature_colname]] == "OTHER")) {
+    formatted_data[formatted_data[[feature_colname]] == "OTHER", ]$color <- "grey"
+    all_phenotypes <- c(cell_types_of_interest, "OTHER")
     all_colours <- c(colour_vector, "grey")
   }
   
   else {
-    all_phenotypes <- types_of_interest
+    all_phenotypes <- cell_types_of_interest
     all_colours <- colour_vector
   }
 
@@ -66,7 +66,7 @@ plot_cell_basic <- function (sce_object, types_of_interest, colour_vector, colum
   plot(formatted_data$Cell.X.Position,formatted_data$Cell.Y.Position,
        pch = 19,cex = cex, col = formatted_data$color, 
        xlab = "X Position", ylab = "Y Position",
-       main = paste("Plot", name_of_object, "by" ,column))
+       main = paste("Plot", name_of_object, "by" ,feature_colname))
   
   x.max <- max(sce_object$Cell.X.Position)
   y.max <- max(sce_object$Cell.Y.Position)
