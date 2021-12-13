69ef81ebb568ef81ca14265d46a16e5e87608521
fuerzhou <653540853@qq.com>
2021-12-13 14:56:16 +1100

    log10->log2;change name; change param "feature_colname"
    Former-commit-id: 81a1f62b8532c67adfe46be6f1f88b86f31799a8
    Former-commit-id: 83665758a9fd94364dd505a0580a33d11adadaff
    Former-commit-id: dd7e5c0d35ac4c0f4be23bdcd0d49a4d20a95773
    Former-commit-id: 784282a41dc58d874a0533ec2691dfd5f0fcb3d9

--- R/entropy.R
+++ R/entropy.R
@@ -1,35 +1,36 @@
-#' entropy
+#' calculate_entropy
 #'
 #' @description Produces a dataframe of entropies for all reference cells or an entropy number for the whole image if a radius is not supplied. 
 #' @param sce_object SingleCellExperiment object in the form of the output of format_image_to_sce
-#' @param types_of_interest Cell.Types of interest, the first celltype is considered as referece celltype
+#' @param cell_types_of_interest Cell.Types of interest, the first celltype is considered as reference celltype
+#' @param feature_colname String specifying the column the markers are from
 #' @param radius (OPTIONAL) The maximum radius around a reference cell for another cell to be considered an interaction.
-#' @param column String specifying the column the markers are from
-#' @importFrom SummarizedExperiment colData 
+#' @importFrom SingleCellExperiment colData 
 #' @return A dataframe or a number depending on the argument radius
 #' @export
-entropy <- function(sce_object, types_of_interest, column, radius = NULL){
-  if((types_of_interest[1] %in% colData(sce_object)[,column]) && 
-     any(types_of_interest[-1] %in% colData(sce_object)[,column])){
+calculate_entropy <- function(sce_object, cell_types_of_interest, feature_colname = "Phenotype", radius = NULL){
+  
+  if((cell_types_of_interest[1] %in% colData(sce_object)[,feature_colname]) && 
+     any(cell_types_of_interest[-1] %in% colData(sce_object)[,feature_colname])){
     if (!is.null(radius)){
-      reference_marker <- types_of_interest[1]
-      target_marker<-types_of_interest
+      reference_marker <- cell_types_of_interest[1]
+      target_marker<-cell_types_of_interest
       n_cells <- number_of_cells_within_radius(sce_object, reference_marker = reference_marker,
-                                               target_marker = target_marker,radius = radius, column = column)
+                                               target_marker = target_marker,radius = radius, feature_colname = feature_colname)
       n_cells.df <- n_cells[[reference_marker]]
       n_cells.df[,"total"] <- 0
       
       for (i in target_marker){
-        n_cells.df[,paste(i,"_log10",sep = "")] <- log10(n_cells.df[,i])
+        n_cells.df[,paste(i,"_log2",sep = "")] <- log2(n_cells.df[,i])
         n_cells.df[,"total"] <- n_cells.df[,"total"]+n_cells.df[,i]
       }
-      n_cells.df[,"total_log"] <- log10(n_cells.df[,"total"])
+      n_cells.df[,"total_log2"] <- log2(n_cells.df[,"total"])
       n_cells.df[n_cells.df == -Inf] <- 0
       
       for (i in target_marker){
         n_cells.df[which(n_cells.df[,"total"] != 0), paste(i,"ratio",sep = "")] <- n_cells.df[,i]/n_cells.df[,"total"]
         n_cells.df[which(n_cells.df[,"total"] == 0), paste(i,"ratio",sep = "")] <- 0
-        n_cells.df[,paste(i,"_entropy",sep = "")] <- n_cells.df[, paste(i,"ratio",sep = "")] *(n_cells.df[,paste(i,"_log10",sep = "")] - n_cells.df[,"total_log"])
+        n_cells.df[,paste(i,"_entropy",sep = "")] <- n_cells.df[, paste(i,"ratio",sep = "")] *(n_cells.df[,paste(i,"_log2",sep = "")] - n_cells.df[,"total_log2"])
       }
       n_cells.df[,"entropy"] <- -(rowSums(n_cells.df[,grepl("_entropy",colnames(n_cells.df))]))  
 
@@ -37,18 +38,18 @@ entropy <- function(sce_object, types_of_interest, column, radius = NULL){
     } else{
       entropy_all <- 0
       data <- data.frame(colData(sce_object))
-      data <- data[which(data[,column] %in% types_of_interest),]
+      data <- data[which(data[,feature_colname] %in% cell_types_of_interest),]
       n_all <- dim(data)[1]
       
       if (n_all == 0){
         return(0.0)
       } else{
-        for (type in types_of_interest){
-          type_data <- data[which(data[,column]==type),]
+        for (type in cell_types_of_interest){
+          type_data <- data[which(data[,feature_colname]==type),]
           n_type <- dim(type_data)[1]
           if (n_type != 0){
             p_type <- n_type/n_all
-            entropy_type <- -(p_type)*log10(p_type)
+            entropy_type <- -(p_type)*log2(p_type)
             entropy_all <- entropy_all + entropy_type 
           }
         }
