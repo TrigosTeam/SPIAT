6b75a39a58d422180b2a60a72da0e4105b02977f
fuerzhou <653540853@qq.com>
2021-12-17 15:28:48 +1100

    add param above
    Former-commit-id: efbfa7465083bbfb5ac685a24474afdab14f181f
    Former-commit-id: 3dda274ce7f379c6c946e6b68b7831ea96cc9132
    Former-commit-id: bde0af4e63331e5848178e19eaaa814ba2af33e7
    Former-commit-id: 80d5363006c20aa1810bcbc3bff41f054e8ff4aa

--- R/calculate_percentage_of_grids.R
+++ R/calculate_percentage_of_grids.R
@@ -7,7 +7,7 @@
 #' @param above Boolean idicating the pattern is above (TRUE) or below (FALSE) the threshold
 #' @export
 
-calculate_percentage_of_grids <- function(raster_obj, threshold){
+calculate_percentage_of_grids <- function(raster_obj, threshold, above){
   raster_obj@data@values[is.na(raster_obj@data@values)] <- 0
   if (above) p <- sum(raster_obj@data@values>=threshold)/length(raster_obj@data@values) * 100
   else p <- sum(raster_obj@data@values<threshold)/length(raster_obj@data@values) * 100
