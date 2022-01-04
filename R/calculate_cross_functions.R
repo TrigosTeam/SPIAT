ef13926143b5396e265d8b1a9032d3ca2ae200c2
fuerzhou <653540853@qq.com>
2022-01-04 17:54:12 +1100

    users can define the `r` param for the cross functions by passing `dist` param
    Former-commit-id: 4f89934be720a9a17bb409bc0d2c0273f17b3fc8 [formerly 9fffb3a49efd23368e8c471462909b93e5237b44]
    Former-commit-id: 70ae5944950fe3fbfbdb176f4ca5aafc5b2b853b
    Former-commit-id: 054feb5a3df1c538c94b444e53e80a96e2722560
    Former-commit-id: 49145a65971533b33a6119529ed658e177ba7f14
    Former-commit-id: 45f5d3394bc03f6e9e31bc1814a9739af4654941

--- R/calculate_cross_functions.R
+++ R/calculate_cross_functions.R
@@ -3,18 +3,22 @@
 #' @description calculate the cross functions between two specified cell types 
 #' 
 #' @param sce_object SingleCellExperiment object in the form of the output of format_image_to_sce
-#' @param method String that is the method for dependence calculation. Options: "Gcross", "Kcross", "Lcross", "Jcross"
+#' @param method (OPTIONAL) String that is the method for dependence calculation. 
+#' Options: "Gcross", "Kcross", "Lcross", "Jcross". Default method is "Kcross"
 #' @param phenotypes Vector of phenotypes of interest
 #' @param feature_colname String that is the name of the column of the types
 #' @param plot_results TRUE if result to be plotted, FALSE if not. In either case, an object with the results is returned
+#' @param dist Number (OPTIONAL) The largest distance between two cell types at which K function is evaluated. 
+#' If NULL, use the default distances set by cross functions. 
 #' @importFrom spatstat.core Gcross Kcross Lcross Jcross
 #' @importFrom spatstat.geom ppp 
 #' @export 
 
 calculate_cross_functions <- function(sce_object, method = "Kcross", 
-                                      phenotypes, feature_colname, plot_results = T) {
-  
+                                      phenotypes, feature_colname, plot_results = T,
+                                      dist = NULL) {
   #CHECK
+  formatted_data <- data.frame(colData(sce_object))
   if (!all(phenotypes %in% formatted_data[[feature_colname]])) {
     stop("Cell type not found!")
   }
@@ -23,30 +27,33 @@ calculate_cross_functions <- function(sce_object, method = "Kcross",
   ppp_object <- format_sce_to_ppp(sce_object)
   ppp_object$marks <- as.factor(ppp_object$marks)
   
+  # r
+  if (is.null(dist)) r <- NULL
+  else r <- seq(0, dist, length.out = 100)
+  
   if (method == "Gcross"){
-    p <- Gcross(ppp_object, phenotypes[1],phenotypes[2],correction = "border")
+    p <- Gcross(ppp_object, phenotypes[1],phenotypes[2],correction = "border", r = r)
     if(plot_results){
       plot(p, main = paste("cross G function",attr(sce_object,"name")))
     }
   }
   if (method == "Kcross"){
-    p <- Kcross(ppp_object, phenotypes[1],phenotypes[2],correction = "border")
+    p <- Kcross(ppp_object, phenotypes[1],phenotypes[2],correction = "border", r = r)
     if(plot_results){
       plot(p, main = paste("cross K function",attr(sce_object,"name")))
     }
   }
   if (method == "Lcross"){
-    p <- Lcross(ppp_object, phenotypes[1],phenotypes[2],correction = "border")
+    p <- Lcross(ppp_object, phenotypes[1],phenotypes[2],correction = "border", r = r)
     if(plot_results){
       plot(p, main = paste("cross L function",attr(sce_object,"name")))
     }
   }
   if (method == "Jcross"){
-    p <- Jcross(ppp_object, phenotypes[1],phenotypes[2],correction = "border")
+    p <- Jcross(ppp_object, phenotypes[1],phenotypes[2],correction = "border", r = r)
     if(plot_results){
       plot(p, main = paste("cross J function",attr(sce_object,"name")))
     }
   }
   return(p)
 }
-
