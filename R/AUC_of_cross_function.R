68654c722c2ef335f1accab0b05bb230cfc53c49
fuerzhou <653540853@qq.com>
2022-01-04 18:08:02 +1100

    normalised AUC

--- R/AUC_of_cross_function.R
+++ R/AUC_of_cross_function.R
@@ -2,12 +2,21 @@
 #'
 #' @description Calculate the difference of area under curve between two curves 
 #'
+#' @param sce_object SingleCellExperiment object that was used for cross K calculation
 #' @param df.cross Dataframe containing the positions of the two curves 
 #' @importFrom pracma trapz
 #' @export
 
 
-AUC_of_cross_function <- function(df.cross){
+AUC_of_cross_function <- function(sce_object, df.cross){
   AUC <- pracma::trapz(df.cross$r ,df.cross$border) - pracma::trapz(df.cross$r ,df.cross$theo)
-  return(AUC)
+  
+  # get the image size
+  data <- data.frame(colData(sce_object))
+  X <- max(data$Cell.X.Position)
+  Y <- max(data$Cell.Y.Position)
+  
+  # calculate normalised AUC
+  n_AUC <- AUC/(X*Y)
+  return(n_AUC)
 }
