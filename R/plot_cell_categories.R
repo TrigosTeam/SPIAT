a3a53283329456c22a2865e886ed1ea774f2236a
fuerzhou <653540853@qq.com>
2022-02-10 21:33:41 +1100

    fix ggplot bug
    Former-commit-id: 00399b2d399b966b7e381de245ec188caec914f3 [formerly 9b442d00655cbcc097d98814b64ae114c8819a27]
    Former-commit-id: 0dfdbbb0e7a2f09463eeaf6580b2dd3d4eb0757c
    Former-commit-id: be5b344d24b7e1bd760ed1d1ffff591ab2fa8327
    Former-commit-id: 8d194a46ab9836303ff8842cb3723f9d5eded547
    Former-commit-id: 610956243c0e8e8b0bfb7f702260963125132638

--- R/plot_cell_categories.R
+++ R/plot_cell_categories.R
@@ -10,7 +10,7 @@
 #' @import dplyr
 #' @import ggplot2
 #' @import tibble
-#' @importFrom SingleCellExperiment colData assay
+#' @importFrom SummarizedExperiment colData assay
 #' @return A plot is returned
 #' @examples
 #' categories_of_interest <- c("AMACR", "CD3,CD8", "PDL-1")
@@ -60,7 +60,8 @@ plot_cell_categories <- function(sce_object, categories_of_interest = NULL,
   
   #set all categories of those that aren't in categories_of_interest to be "OTHER"
   if (any(!formatted_data[[feature_colname]] %in% categories_of_interest)) {
-    formatted_data[!formatted_data$Phenotype %in% categories_of_interest,][[feature_colname]] <- "OTHER"
+    formatted_data[!formatted_data[[feature_colname]] %in% categories_of_interest,
+    ][[feature_colname]] <- "OTHER"
   }
   
   #Assign the colour to corresponding phenotypes in df
@@ -69,6 +70,7 @@ plot_cell_categories <- function(sce_object, categories_of_interest = NULL,
     idx <- which(categories_of_interest == category)
     formatted_data[formatted_data[[feature_colname]] == category, ]$color <- colour_vector[idx]
   }
+  
   if (any(formatted_data[[feature_colname]] == "OTHER")) {
     formatted_data[formatted_data[[feature_colname]] == "OTHER", ]$color <- "lightgrey"
     all_categories <- c(categories_of_interest, "OTHER")
@@ -77,31 +79,33 @@ plot_cell_categories <- function(sce_object, categories_of_interest = NULL,
     all_categories <- categories_of_interest
     all_colours <- colour_vector
   }
-    
-  p <- ggplot(formatted_data, aes_string(x = "Cell.X.Position", y = "Cell.Y.Position", colour = feature_colname)) +
+  
+  p <- ggplot(formatted_data, aes_string(x = "Cell.X.Position", y = "Cell.Y.Position")) +
     geom_point(aes_string(colour = feature_colname), size = 1)
-  p <- ggplot(formatted_data, aes_string(x = "Cell.X.Position", y = "Cell.Y.Position", colour = feature_colname))
-  if (any(formatted_data[[feature_colname]] == "OTHER")) {
-    p <- p + geom_point(data=subset(formatted_data, get(feature_colname) =='OTHER'), 
-                        aes_string(colour = feature_colname), size = 1) + 
-      geom_point(data=subset(formatted_data, get(feature_colname) !='OTHER'), 
-                 aes_string(colour = feature_colname), size = 1) 
-  }else{
-    p <- p + geom_point(aes_string(colour = feature_colname), size = 1)}
-    
+  # p <- ggplot(formatted_data, aes_string(x = "Cell.X.Position", y = "Cell.Y.Position", colour = "color"))
+  # if (any(formatted_data[[feature_colname]] == "OTHER")) {
+  #   p <- p + geom_point(data=subset(formatted_data, get(feature_colname) =='OTHER'),
+  #                       aes_string(colour = "color"), size = 1) +
+  #     geom_point(data=subset(formatted_data, get(feature_colname) !='OTHER'),
+  #                aes_string(colour = "color"), size = 1)
+  # }else{
+  #   p <- p + geom_point(aes_string(colour = "color"), size = 1)}
+  # 
   p <- p +
     guides(alpha = "none") +
-    labs(colour = feature_colname) + 
-    scale_color_manual(breaks = all_categories, values=all_colours) +
-    theme(panel.grid.major = element_blank(),
-          panel.grid.minor = element_blank(),
-          panel.background = element_rect(fill = "white"),
-          axis.title.x = element_blank(),
-          axis.text.x = element_blank(),
-          axis.ticks.x = element_blank(),
-          axis.title.y = element_blank(),
-          axis.text.y = element_blank(),
-          axis.ticks.y = element_blank())
+    ggtitle(paste("Plot", attr(sce_object, "name"), feature_colname, sep = " ")) +
+    scale_color_manual(breaks = all_categories, values=all_colours)
+  # labs(colour = all_categories) +
+  
+  #   theme(panel.grid.major = element_blank(),
+  #         panel.grid.minor = element_blank(),
+  #         panel.background = element_rect(fill = "white"),
+  #         axis.title.x = element_blank(),
+  #         axis.text.x = element_blank(),
+  #         axis.ticks.x = element_blank(),
+  #         axis.title.y = element_blank(),
+  #         axis.text.y = element_blank(),
+  #         axis.ticks.y = element_blank())
   
   print(p)
 }
