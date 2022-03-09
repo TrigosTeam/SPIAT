d7c3c21cc297e79a3f4a96d669ad73527f70332c
“fuerzhou” <653540853@qq.com>
2022-03-09 17:36:37 +1100

    update to standard format
    Former-commit-id: d336a6847d6d5f35a62fe7f6fe1a27f560eb6d7e
    Former-commit-id: 5806436ebb269af3c3caae6f9afe6f64c4de5c03
    Former-commit-id: e7c602579b5a5335a92810f964626af176737ca8
    Former-commit-id: 8ef4508f74dc165f3ceca36eecc0a1829d7c28d2

--- R/calculate_minimum_distances_between_cell_types.R
+++ R/calculate_minimum_distances_between_cell_types.R
@@ -1,19 +1,16 @@
 #' calculate_minimum_distances_between_cell_types
 #'
-#' @description Returns the mean, median and stardard deviation of the minimum 
-#' distances between phenotypes
-#' @param sce_object SingleCellExperiment object in the form of the output of format_image_to_sce
-#' @param all_combinations If TRUE, the distances between all possible combinations of cell types
-#' will be calculated
-#' @param cell_types_of_interest Vector of cell types to consider if all_combinations is FALSE
-#' @param feature_colname String Column of cells to choose the cell type from (e.g. Cell.Type, Cell.Type2, etc)
-#' @importFrom apcluster negDistMat
-#' @importFrom RANN nn2
-#' @importFrom gtools permutations
+#' @description Returns the mean, median and stardard deviation of the minimum
+#'   distances between phenotypes.
+#' @param sce_object SingleCellExperiment object in the form of the output of
+#'   format_image_to_sce.
+#' @param all_combinations Boolean. If TRUE, the distances between all possible
+#'   combinations of cell types will be calculated.
+#' @param cell_types_of_interest String Vector of cell types to consider if
+#'   all_combinations is FALSE.
+#' @param feature_colname String. Column of cells to choose the cell type from
+#'   (e.g. Cell.Type, Cell.Type2, etc).
 #' @import dplyr
-#' @importFrom stats median sd
-#' @importFrom SingleCellExperiment colData
-#' @importFrom tibble rownames_to_column
 #' @return A data.frame is returned
 #' @examples
 #' summary_distances <- calculate_summary_distances_between_cell_types(SPIAT::formatted_image)
@@ -24,8 +21,7 @@ calculate_minimum_distances_between_cell_types <- function(sce_object,
                                                            all_combinations = FALSE,
                                                            cell_types_of_interest) {
   
-  formatted_data <- data.frame(colData(sce_object))
-  formatted_data <- formatted_data %>% rownames_to_column("Cell.ID") #convert rowname to column
+  formatted_data <- get_colData(sce_object)
   
   formatted_data <- formatted_data[,c("Cell.ID","Cell.X.Position", "Cell.Y.Position", feature_colname)]
   formatted_data <- formatted_data[formatted_data[,feature_colname] != "",]
@@ -55,7 +51,7 @@ calculate_minimum_distances_between_cell_types <- function(sce_object,
   }
   
   #permutation for the different cell type combinations
-  comb = permutations(length(unique(formatted_data[[feature_colname]])), 2, repeats.allowed = T)
+  comb = gtools::permutations(length(unique(formatted_data[[feature_colname]])), 2, repeats.allowed = T)
   unique_cells <- unique(formatted_data[[feature_colname]]) #unique cell types
   result = vector()
   
@@ -78,11 +74,11 @@ calculate_minimum_distances_between_cell_types <- function(sce_object,
       all_celltype1_cord <- formatted_data[formatted_data[,feature_colname] == name1, c("Cell.X.Position", "Cell.Y.Position")]
       
       #find all of closest points
-      all_closest <- nn2(data = all_celltype2_cord, query = all_celltype1_cord, k = 1)
+      all_closest <- RANN::nn2(data = all_celltype2_cord, query = all_celltype1_cord, k = 1)
       
       local_dist_mins <- all_closest$nn.dists
       
-      local_result <- data.frame(Reference = name1, Nearest = name2, Mean = mean(local_dist_mins), Std.Dev = sd(local_dist_mins), Median = median(local_dist_mins))
+      local_result <- data.frame(Reference = name1, Nearest = name2, Mean = mean(local_dist_mins), Std.Dev = stats::sd(local_dist_mins), Median = stats::median(local_dist_mins))
     }
     result <- rbind(result, local_result)
   }
