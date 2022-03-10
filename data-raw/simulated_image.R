befee93d864bbbc6506bc032355e0136c73cd9d0
“fuerzhou” <653540853@qq.com>
2022-03-11 00:24:29 +1100

    updated simulated image to have fewer background tumour cells
    Former-commit-id: 3a99b4a38c5220d7c3c2aec22aea47aea965f05c

--- data-raw/simulated_image.R
+++ data-raw/simulated_image.R
@@ -7,7 +7,7 @@ library(SPIAT)
 set.seed(610)
 bg <- simulate_mixing(names_of_mixing = c("Tumour", "Immune1", "Immune2", 
                                           "Immune", "Others"),
-                      mixing_degree =c(0.1, 0.1, 0.05, 0.02, 0.73),
+                      mixing_degree =c(0.03, 0.1, 0.05, 0.02, 0.8),
                       plot.image = FALSE)
 # Tumour: AMACR
 # Immune1: CD3,CD4
@@ -18,14 +18,14 @@ bg <- simulate_mixing(names_of_mixing = c("Tumour", "Immune1", "Immune2",
 set.seed(610)
 a <- multiple_images_with_immune_rings(
     background_sample = bg, cluster_size = 150, ring_width = 80,
-    ring_infiltration = 0.3, infiltration = 0.2, cluster_loc_x = 50, 
+    ring_infiltration = 0.3, infiltration = 0.1, cluster_loc_x = 50, 
     cluster_loc_y = -100, plot.image = FALSE
 )
 a <- a[[1]]
 set.seed(610)
 a <- multiple_images_with_immune_rings(
     background_sample = a, ring_shape = 2, cluster_size = 120, ring_width = 80,
-    ring_infiltration = 0.3, infiltration = 0.3, cluster_loc_x = -500, 
+    ring_infiltration = 0.3, infiltration = 0.1, cluster_loc_x = -500, 
     cluster_loc_y = 500, plot.image = FALSE
 )
 a <- a[[1]]
@@ -37,33 +37,33 @@ plot_cell_categories(a, c("Tumour","Immune1", "Immune2", "Immune"),
 
 # simulate markers 
 table(a$Cell.Type)
-#Immune Immune1 Immune2  Others  Tumour 
-#757     359     159    2754     922 
+# Immune Immune1 Immune2  Others  Tumour 
+# 630     338     178    2986     819 
 
 
 n_cells <- sum(table(a$Cell.Type))
 set.seed(610)
 AMACR <- rbeta(n_cells, 0.25, 0.78)
 plot(density(AMACR))
-AMACR_p <- AMACR > 0.55
+AMACR_p <- AMACR > 0.6
 table(AMACR_p)
 
 set.seed(610)
-CD3 <- rbeta(n_cells, 0.7, 25)
+CD3 <- rbeta(n_cells, 0.07, 0.5)
 plot(density(CD3))
-CD3_p <- CD3 > 0.05
+CD3_p <- CD3 > 0.2
 table(CD3_p)
 
 set.seed(610)
-CD4 <- rbeta(n_cells, 1, 20)
+CD4 <- rbeta(n_cells, 0.08, 0.6)
 plot(density(CD4))
-CD4_p <- CD4 > 0.07
+CD4_p <- CD4 > 0.2
 table(CD4_p)
 
 set.seed(610)
-CD8 <- rbeta(n_cells, 1, 300)
+CD8 <- rbeta(n_cells, 0.08, 3)
 plot(density(CD8))
-CD8_p <- CD8 > 0.01
+CD8_p <- CD8 > 0.2
 table(CD8_p)
 
 FOXP3 <- rbeta(n_cells, 1, 200)
@@ -80,42 +80,42 @@ data$pseudo <- NULL
 
 # AMACR
 set.seed(610)
-AMACR_intensities <- sample(AMACR[AMACR_p], 922, replace = TRUE)
+AMACR_intensities <- sample(AMACR[AMACR_p], 819, replace = TRUE)
 data[data$Cell.Type == "Tumour", "AMACR"] <- AMACR_intensities
 set.seed(610)
-NoAMACR_intensities <- sample(AMACR[!AMACR_p], n_cells-922, replace = TRUE)
+NoAMACR_intensities <- sample(AMACR[!AMACR_p], n_cells-819, replace = TRUE)
 data[data$Cell.Type != "Tumour", "AMACR"] <- NoAMACR_intensities
 
 #CD3
 set.seed(610)
-CD3_intensities <- sample(CD3[CD3_p], 1275, replace = TRUE)
+CD3_intensities <- sample(CD3[CD3_p], 1146, replace = TRUE)
 data[data$Cell.Type %in% c("Immune","Immune1","Immune2"), "CD3"] <- CD3_intensities
 set.seed(610)
-NoCD3_intensities <- sample(CD3[!CD3_p], n_cells-1275, replace = TRUE)
+NoCD3_intensities <- sample(CD3[!CD3_p], n_cells-1146, replace = TRUE)
 data[!data$Cell.Type %in% c("Immune","Immune1","Immune2"), "CD3"] <- NoCD3_intensities
 
 #CD4
 set.seed(610)
-CD4_intensities <- sample(CD4[CD4_p], 1116, replace = TRUE)
+CD4_intensities <- sample(CD4[CD4_p], 968, replace = TRUE)
 data[data$Cell.Type %in% c("Immune","Immune1"), "CD4"] <- CD4_intensities
 set.seed(610)
-NoCD4_intensities <- sample(CD4[!CD4_p], n_cells-1116, replace = TRUE)
+NoCD4_intensities <- sample(CD4[!CD4_p], n_cells-968, replace = TRUE)
 data[!data$Cell.Type %in% c("Immune","Immune1"), "CD4"] <- NoCD4_intensities
 
 #CD8
 set.seed(610)
-CD8_intensities <- sample(CD8[CD8_p], 159, replace = TRUE)
+CD8_intensities <- sample(CD8[CD8_p], 178, replace = TRUE)
 data[data$Cell.Type == "Immune2", "CD8"] <- CD8_intensities
 set.seed(610)
-NoCD8_intensities <- sample(CD8[!CD8_p], n_cells-159, replace = TRUE)
+NoCD8_intensities <- sample(CD8[!CD8_p], n_cells-178, replace = TRUE)
 data[data$Cell.Type != "Immune2", "CD8"] <- NoCD8_intensities
 
 #FOXP3
 set.seed(610)
-FOXP3_intensities <- sample(FOXP3[FOXP3_p], 757, replace = TRUE)
+FOXP3_intensities <- sample(FOXP3[FOXP3_p], 630, replace = TRUE)
 data[data$Cell.Type == "Immune", "FOXP3"] <- FOXP3_intensities
 set.seed(610)
-NoFOXP3_intensities <- sample(FOXP3[!FOXP3_p], n_cells-757, replace = TRUE)
+NoFOXP3_intensities <- sample(FOXP3[!FOXP3_p], n_cells-630, replace = TRUE)
 data[data$Cell.Type != "Immune", "FOXP3"] <- NoFOXP3_intensities
 
 # define the orginial phenotypes
