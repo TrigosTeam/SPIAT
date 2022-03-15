3a99cb531e01b08cf9684b93e633815750af176b
fuerzhou <653540853@qq.com>
2022-03-15 16:32:31 +1100

    only keep "Cluster" as aggregate type
    Former-commit-id: 6e64a55f62d37712160cc1b671da3f2995c6eff2 [formerly 4d226e8a30356d160a3bcb0952a1b1f7ed1549dc]
    Former-commit-id: cd22424ec82af55ed9668b7f4b7f1fac30ab251f
    Former-commit-id: b05573dbce515ad858fdf5566fa9da5b18893771
    Former-commit-id: fd13573e25f81fce6228331bf66d8f6862c2d769
    Former-commit-id: 76da802cacffd5a1253a7c7a0b0b598458dddc04

--- R/plot_composition_heatmap.R
+++ R/plot_composition_heatmap.R
@@ -5,37 +5,23 @@
 #' @param composition - a dataframe output from cluster_percent_composition
 #' @param pheno_to_exclude Vector of phenotype to exclude
 #' @param log_values TRUE if the percentages should be logged (base 10)
-#' @param type_of_aggregate Cluster or Community
 #' @param feature_colname String. Column with cell types.
 #' @return A plot is returned
 #' @examples
 #' communities <- identify_cell_communities(SPIAT::formatted_image, radius=100)
-#' communities_vis <- composition_of_clusters_and_communities(communities, type_of_aggregate = "Community", feature_colname="Phenotype)
-#' plot_composition_heatmap(communities_vis, type_of_aggregate = "Community", feature_colname="Phenotype")
+#' communities_vis <- composition_of_clusters_and_communities(communities, feature_colname="Phenotype)
+#' plot_composition_heatmap(communities_vis, feature_colname="Phenotype")
 #' @export
 
-plot_composition_heatmap <- function(composition, pheno_to_exclude = NULL, log_values = FALSE, type_of_aggregate, feature_colname) {
+plot_composition_heatmap <- function(composition, pheno_to_exclude = NULL, log_values = FALSE, feature_colname) {
 
-  if(type_of_aggregate == "Community"){
-    cluster_size <- unique(data.frame(Community = composition$Community,
-                                      Total_cells = composition$Total_number_of_cells))
-    rownames(cluster_size) <- cluster_size$Community
-    cluster_size$Community <- NULL
-
-    composition2 <- composition[,c(feature_colname, "Community", "Percentage")]
-    composition2 <- reshape2::dcast(composition2, paste(feature_colname, "~", type_of_aggregate), value.var="Percentage")
-
-  }else if(type_of_aggregate == "Cluster"){
     cluster_size <- unique(data.frame(Cluster = composition$Cluster,
                                       Total_cells = composition$Total_number_of_cells))
     rownames(cluster_size) <- cluster_size$Cluster
     cluster_size$Cluster <- NULL
 
     composition2 <- composition[,c(feature_colname, "Cluster", "Percentage")]
-    composition2 <- reshape2::dcast(composition2, paste(feature_colname, "~", type_of_aggregate), value.var="Percentage")
-  }else{
-    stop("Only Community and Cluster are accepted as valid colname names")
-  }
+    composition2 <- reshape2::dcast(composition2, paste(feature_colname, "~", "Cluster"), value.var="Percentage")
 
 
   rownames(composition2) <- composition2[,feature_colname]
