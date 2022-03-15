0e9680dd22a405ed8d6ff4e9aced1dff1745c56b
fuerzhou <653540853@qq.com>
2022-03-15 16:33:31 +1100

    add new image (no markers)
    Former-commit-id: 1d3ce4d4d7ca7cf7181e2d01b3a84c2c3dc2765d
    Former-commit-id: 0eccb685cff78045c7c71bb0e0f948706252b8c7
    Former-commit-id: 7b8ef69a859af2a17ebffe98fce0afc584956a70
    Former-commit-id: 97808664808bcdd31d83e2325896bfae081ac0f5

---
+++ data-raw/image_no_markers.R
@@ -0,0 +1,56 @@
+## code to prepare `image_no_markers` dataset goes here
+#####
+library(spaSim)
+library(SPIAT)
+
+# simulate an image with three immune clusters
+set.seed(610)
+bg <- simulate_mixing(names_of_mixing = c("Tumour", "Immune1", "Immune2", 
+                                          "Immune", "Others"),
+                      mixing_degree =c(0.03, 0.1, 0.05, 0.02, 0.8),
+                      plot.image = FALSE)
+
+set.seed(610)
+a <- simulate_clusters(background_sample = bg,
+                       n_clusters = 4, properties_of_clusters = list(
+                           C1 = list(name_of_cluster_cell = "Immune1", size = 900,
+                                     shape = "Irregular", centre_loc = data.frame(x = 200, y = 200), 
+                                     infiltration_types = c("Immune","Immune2", "Others"), 
+                                     infiltration_proportions = c(0.3, 0.2, 0.1)),
+                           C2 = list(name_of_cluster_cell = "Immune2", size = 1000,
+                                     shape = "Irregular", centre_loc = data.frame(x = 1200, y = 400), 
+                                     infiltration_types = c("Immune", "Others"), 
+                                     infiltration_proportions = c(0.2, 0.05)),
+                           C3 = list(name_of_cluster_cell = "Immune", size = 800, 
+                                     shape = "Irregular", centre_loc = data.frame(x = 500, y = 1200), 
+                                     infiltration_types = c("Immune1", "Immune2", "Others"), 
+                                     infiltration_proportions = c(0.2, 0.3, 0.05)),
+                           C4 = list(name_of_cluster_cell = "Immune", size = 280, 
+                                     shape = "Oval", centre_loc = data.frame(x = 800, y = 1200), 
+                                     infiltration_types = c("Immune1", "Immune2", "Others"), 
+                                     infiltration_proportions = c(0.2, 0.3, 0.05))),
+                       plot.image = TRUE)
+
+
+a$Cell.Type <- a$Phenotype
+a$Phenotype <- NULL
+plot_cell_categories(a, c("Tumour","Immune1", "Immune2", "Immune"), 
+                     c("red","darkblue", "darkgreen", "brown"), "Cell.Type")
+
+# simulate markers 
+table(a$Cell.Type)
+# Immune Immune1 Immune2  Others  Tumour 
+# 499     600     674    3059     119 
+sum(table(a$Cell.Type))
+
+
+# format to sce
+intensity_matrix <- NULL
+coord_x <- a$Cell.X.Position
+coord_y <- a$Cell.Y.Position
+
+image_no_markers <- format_colData_to_sce(a)
+plot_cell_categories(image_no_markers, c("Tumour","Immune1", "Immune2", "Immune"), 
+                     c("red","darkblue", "darkgreen", "brown"), "Cell.Type")
+#####
+usethis::use_data(image_no_markers, overwrite = TRUE)
