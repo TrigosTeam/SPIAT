6af12b998c4178deb0930d1c807fc15c13f3ad0d
fuerzhou <653540853@qq.com>
2022-03-15 12:11:31 +1100

    change the fill of the stacked bar plot; change param to cells_to_exclude (not just tumour cells to exclude)
    Former-commit-id: f98937152280812cf25575205c77a181f1461f96

--- R/plot_cell_percentages.R
+++ R/plot_cell_percentages.R
@@ -1,18 +1,18 @@
 #' plot_cell_proportions
 #'
-#' @description Plots cells proportions as barplots
-#' @param cell_proportions Output from calculate_cell_proportions
-#' @param tumour_marker Tumour marker to exclude if needed
+#' @description Plots cells proportions as barplots.
+#' @param cell_proportions Data Frame. Output from `calculate_cell_proportions`.
+#' @param tumour_marker String. Tumour marker to exclude if needed.
 #' @param cellprop_colname Column to use for y axis names. Default is "Proportion_name"
 #' @import ggplot2
+#' @import dplyr
 #' @return A plot is returned
 #' @examples
 #' p_cells <- calculate_cell_proportions(SPIAT::formatted_image, reference_celltypes=c("Total", "CD3"))
 #' plot_cell_percentages(p_cells)
 #' @export
-
-
-plot_cell_percentages <- function(cell_proportions, tumour_marker=NULL, cellprop_colname="Proportion_name"){
+plot_cell_percentages <- function(cell_proportions, cells_to_exclude =NULL, 
+                                  cellprop_colname="Proportion_name"){
   
   # setting these variables to NULL as otherwise get "no visible binding for global variable" in R check
   Cell_type <- Percentage <- Percentage_label <- NULL
@@ -21,30 +21,33 @@ plot_cell_percentages <- function(cell_proportions, tumour_marker=NULL, cellprop
 
   cell_proportions$Percentage_label <- round(cell_proportions$Percentage, digits=1)
   cell_proportions <- cell_proportions[cell_proportions$Cell_type != "OTHER",]
-
+  
   cell_percentages_full_plot <-
-    ggplot(cell_proportions,aes(x = stats::reorder({{cellprop_colname}}, Percentage), y = Percentage)) +
-      geom_bar(stat = 'identity', fill = "#bababa") +
+    ggplot(cell_proportions,aes(x = stats::reorder({{cellprop_colname}}, Percentage), 
+                                y = Percentage, fill = Cell_type)) +
+      geom_bar(stat = 'identity') +
       theme_bw() +
-      theme(legend.position = "none") +
+      theme() +
       xlab("Cell Type") + ylab("Proportion of cells") +
-      geom_text(aes(label = Percentage_label), position=position_dodge(width=0.9), hjust=-0.25, size = 3) +
+      geom_text(aes(label = Percentage_label), 
+                position = position_stack(vjust = 0.5),size = 3) +
     coord_flip()
   
-  if(!is.null(tumour_marker)){
-    cell_proportions_no_tumour <- cell_proportions[cell_proportions$Cell_type != tumour_marker,]
+  if(!is.null(cells_to_exclude)){
+    cell_proportions_no_tumour <- cell_proportions[!cell_proportions$Cell_type %in% cells_to_exclude,]
     cell_proportions_no_tumour$Proportion <- NULL
-    cell_proportions_no_tumour$Percentage <- (cell_proportions_no_tumour$Number_of_cells/sum(cell_proportions_no_tumour$Number_of_cells))*100
+    cell_proportions_no_tumour$Percentage <- (cell_proportions_no_tumour$Number_of_celltype/sum(cell_proportions_no_tumour$Number_of_celltype))*100
     cell_proportions_no_tumour$Percentage_label <- round(cell_proportions_no_tumour$Percentage, digits=1)
 
     cell_percentages_no_tumour_plot <-
-      ggplot(cell_proportions_no_tumour, aes(x = stats::reorder({{cellprop_colname}}, Percentage), y = Percentage)) +
-      geom_bar(stat = 'identity', fill = "#bababa") +
-      ggtitle("Excluding tumor cells")+
+      ggplot(cell_proportions_no_tumour, aes(x = stats::reorder({{cellprop_colname}}, Percentage), 
+                                             y = Percentage, fill=Cell_type)) +
+      geom_bar(stat = 'identity') +
+      ggtitle(paste("Excluding cells:",cells_to_exclude))+
       theme_bw() +
-      theme(legend.position = "none") +
+      theme() +
       xlab("Cell Type") + ylab("Proportion of cells") +
-      geom_text(aes(label = Percentage_label), position=position_dodge(width=0.9), hjust=-0.25, size = 2) +
+      geom_text(aes(label = Percentage_label), position = position_stack(vjust = 0.5), size = 2) +
       coord_flip()
     print(cell_percentages_no_tumour_plot)
   }
