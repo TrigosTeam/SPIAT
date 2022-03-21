7dd0faf693be3f9dfa7750ce9384847e85a9df54
fuerzhou <653540853@qq.com>
2022-03-21 17:23:12 +1100

    add/update tests
    Former-commit-id: 815a7ee3b8f6f644b703c70611dcac7156f836f8
    Former-commit-id: e74122d41c3e443cdfa4eb0b3b8acd61bfc100ec
    Former-commit-id: 20d9a786807bda5f45a0ed3b0775bd2e1840ba9c
    Former-commit-id: f3267d1683eac6a1b7849b68618b7422ca61c5e1
    Former-commit-id: cd279980979342c3daad67a4ce7735790bfe7fd5

--- tests/testthat/test-utilities.R
+++ tests/testthat/test-utilities.R
@@ -1,6 +1,6 @@
 context("utilities")
 
-test_that("format_image_to_sce() works", {
+test_that("format_image_to_sce() works for inForm data", {
   
   raw_inform_data <- system.file("extdata", "tiny_inform.txt.gz", package = "SPIAT")
   markers <- c("DAPI","CD3","PDL-1","CD4","CD8","AMACR")
@@ -24,47 +24,119 @@ test_that("format_image_to_sce() works", {
   expect_equal(dim(sce), c(6L, 9L))
 })
 
+test_that("format_sce_to_ppp() wroks", {
+  
+  ppp_object <- format_sce_to_ppp(defined_image, feature_colname = "Cell.Type")
+  
+  expect_is(ppp_object, "ppp")
+  expect_equal(ppp_object$x[1], 139.8, tolerance = 0.1)
+  expect_equal(ppp_object$y[1], 86.7, tolerance = 0.1)
+  expect_equal(ppp_object$marks[1], "Others")
+  
+})
 
-test_that("print_phenotypes works", {
+test_that("format_colData_to_sce() wroks", {
+  
+  df <- data.frame(Cell.ID = c("Cell_1", "Cell_2"), Cell.X.Positions = c(2,5), 
+                   Cell.Y.Positions = c(3.3, 8), Phenotypes = c("CD3", "CD3,CD8"))
+  sce <- format_colData_to_sce(df)
+  
+  out <- data.frame(SummarizedExperiment::colData(sce))
+  
+  expect_s4_class(sce, "SingleCellExperiment")
+  expect_equal(dim(sce)[1], 1)
+  expect_equal(dim(out)[2], 5)
+})
+
+test_that("print_feature works", {
     
-    res <- c("OTHER", "AMACR", "CD3,CD4", "CD3,CD8", "CD3", "PDL-1")
+    res <- c("OTHER","Immune_marker1,Immune_marker2", "Tumour_marker" , 
+             "Immune_marker1,Immune_marker2,Immune_marker4", "Immune_marker1,Immune_marker3"   )
     
-    phenotypes <- print_phenotypes(formatted_image)
+    phenotypes <- print_feature(simulated_image, "Phenotype")
   
     expect_equal(phenotypes, res)
 })
 
-
 test_that("select_phenotypes works", {
     
-    phenotypes_keep <- c("AMACR", "CD3")
+    phenotypes_keep <- c("Immune_marker1,Immune_marker2", "Tumour_marker")
     
-    image_subset <- select_phenotypes(formatted_image, keep=TRUE,
-                                         phenotypes = phenotypes_keep)
-    phenotypes <- unique(colData(image_subset)$Phenotype)
+    data_subset <- select_celltypes(simulated_image,
+                                    celltypes = c("Tumour_marker","Immune_marker1,Immune_marker2"),
+                                    feature_colname = "Phenotype", keep=TRUE)
+    phenotypes <- unique(colData(data_subset)$Phenotype)
     
     expect_equal(phenotypes, phenotypes_keep)
 })
 
+test_that("define_celltypes works", {
+  
+  celltypes_defined <- c("Others", "Immune1", "Tumour", "Immune3", "Immune2")
+  
+  defined_sce <- define_celltypes(simulated_image, 
+                                  categories = c("Tumour_marker", "Immune_marker1,Immune_marker2", "Immune_marker1,Immune_marker3", 
+                                                 "Immune_marker1,Immune_marker2,Immune_marker4", "OTHER"), category_colname = "Phenotype",
+                                  names = c("Tumour", "Immune1", "Immune2","Immune3", "Others"), new_colname = "Cell.Type")
+  celltypes <- unique(colData(defined_sce)$Cell.Type)
+  
+  expect_equal(celltypes, celltypes_defined)
+})
 
-#test_that("image_splitter works", {
-#    
-#    res <-    data.frame(
-#      row.names = c("Cell_1", "Cell_2", "Cell_3", "Cell_4"),
-#      Phenotype = c("OTHER", "AMACR", "AMACR", "AMACR"), 
-#      Cell.X.Position = c(82, 171, 184, 201), 
-#      Cell.Y.Position = c(30, 22, 38, 52), 
-#      Cell.Area = c(477, 464, 553, 462), 
-#      Nucleus.Area = c(160,177, 212, 239), 
-#      Nucleus.Compactness = c(0.52, 0.54, 0.51, 0.53), 
-#      Nucleus.Axis.Ratio = c(2.05, 1.84, 1.92, 1.47), 
-#      Cell.Axis.Ratio = c(1.48, 1, 1.21, 1.09), 
-#      Cell_type = c("OTHER", "AMACR", "AMACR", "AMACR"))
-#    
-#    split_image <- image_splitter(formatted_image, number_of_splits=3)
-#    
-#    expect_is(split_image, "list")
-#    expect_equal(length(split_image), 9)
-#    expect_equal(split_image[[1]][1:4, ], res)
-#})
+test_that("image_splitter works", {
 
+   res <-    data.frame(
+     row.names = c("Cell_1", "Cell_2", "Cell_3", "Cell_4"),
+     Phenotype = c("OTHER", "OTHER", "OTHER", "OTHER"),
+     Cell.X.Position = c(139.77484, 77.86721, 84.44626, 110.19857),
+     Cell.Y.Position = c(86.704079, 80.096527, 19.238638, 5.656004))
+
+   split_image <- image_splitter(simulated_image, number_of_splits=3, plot = FALSE)
+   
+   out <- split_image[[1]][1:4, ]
+
+   expect_is(split_image, "list")
+   expect_equal(length(split_image), 9)
+   expect_equal(out, res)
+})
+
+test_that("predict_phenotypes works", {
+  
+  # test it works when reference_phenotypes = TRUE
+  res <-    data.frame(
+    row.names = c(1L, 2L),
+    Cell.ID = c("Cell_1","Cell_2"),
+    Tumour_marker_actual_phenotype = c(0,0),
+    Immune_marker1_actual_phenotype = c(0,0),
+    Immune_marker2_actual_phenotype = c(0,0),
+    Immune_marker3_actual_phenotype = c(0,0),
+    Immune_marker4_actual_phenotype = c(0,0),
+    Immune_marker1_predicted_phenotype = c(0,0),
+    Immune_marker2_predicted_phenotype = c(0,0),
+    Immune_marker3_predicted_phenotype = c(0,0),
+    Immune_marker4_predicted_phenotype = c(0,0),
+    Tumour_marker_predicted_phenotype = c(1,0))
+  
+  predicted_result <- predict_phenotypes(sce_object = simulated_image, thresholds = NULL,
+                                         tumour_marker = "Tumour_marker",
+                                         baseline_markers = c("Immune_marker1", "Immune_marker2", 
+                                                              "Immune_marker3", "Immune_marker4"), 
+                                         reference_phenotypes = TRUE)
+  
+  out <- predicted_result[1:2,c(1,10:19)]
+  
+  expect_is(predicted_result, "data.frame")
+  expect_equal(dim(predicted_result), c(4951, 19))
+  expect_equal(out, res)
+  
+  # test it works when reference_phenotypes = FALSE
+  predicted_sce_image <- predict_phenotypes(sce_object = simulated_image, thresholds = NULL,
+                                            tumour_marker = "Tumour_marker",
+                                            baseline_markers = c("Immune_marker1", "Immune_marker2", 
+                                                                 "Immune_marker3", "Immune_marker4"), 
+                                            reference_phenotypes = FALSE)
+  
+  predicted_phenotypes <- unique(predicted_sce_image$Phenotype)
+  
+  expect_length(predicted_phenotypes, 27)
+})
