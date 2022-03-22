ba811d6284bca38b1c7a5b01d59500fc132c7b9d
fuerzhou <653540853@qq.com>
2022-03-22 13:51:35 +1100

    add documentation for datasets

---
+++ R/image_no_markers.R
@@ -0,0 +1,11 @@
+#' SCE object of a formatted image without marker intensities (simulated by
+#' `spaSim` package)
+#'
+#' A dataset that contains a formatted sce object with cell ids, cell positions,
+#' and cell types in metadata. This dataset does not contain assays (marker
+#' intensities).
+#'
+#' @format An sce object. colData contains 4951 rows (cells) and 4 columns
+#'   (metadata).
+#' @seealso \code{\link{defined_image}} \code{\link{simulated_image}}
+"image_no_markers"
