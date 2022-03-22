75344249f36b5f6338e3c4112ee000f35a8be34a
fuerzhou <653540853@qq.com>
2022-03-22 11:11:20 +1100

    change column name; add min and max heatmap
    Former-commit-id: ddd1e5d09417c19a1ef8235d6aa810e094c27f6a [formerly 5263548b5ab3dc80f8b6aa94d62a93bd9f0e72af]
    Former-commit-id: 2bb4f032894f74cb7aec45cab2a71e49661564c6
    Former-commit-id: f2c8c9ff8879525b8ba96b1a114bb2ef66b4934b
    Former-commit-id: ed675945ec94baad8ae58d1c926af2ae115813ca
    Former-commit-id: c64340307a19b4ddbd159b3c82e49a08d057cc36
    Former-commit-id: 6597c202ff1827fd082d47a5d567b5adcb5becd0

--- R/plot_distance_heatmap.R
+++ R/plot_distance_heatmap.R
@@ -2,7 +2,7 @@
 #'
 #' @description Takes the output of cell_distances and plot the distances as a heatmap
 #'
-#' @param phenotype_distances_result Dataframe output from calculate_distances_between_phenotypes
+#' @param phenotype_distances_result Dataframe output from calculate_summary_distances_between_phenotypes
 #' @param metric Metric to be plotted. One of "mean", "std.dev" or "median".
 #' @import ggplot2
 #' @return A plot is returned
@@ -19,10 +19,10 @@ plot_distance_heatmap <- function(phenotype_distances_result, metric = "mean"){
 
     if(metric == "mean"){
       limit <- range(unlist(phenotype_distances_result$Mean), na.rm=TRUE)
-      g <- ggplot(phenotype_distances_result, aes(x = Reference, y = Nearest, fill = Mean)) +
+      g <- ggplot(phenotype_distances_result, aes(x = Reference, y = Target, fill = Mean)) +
         geom_tile() +
-        xlab("Cell of interest (COI)") +
-        ylab("Nearest cell to COI") +
+        xlab("Reference cell type") +
+        ylab("Target cell type") +
         theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_rect(fill = "white"),
               axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
         scale_fill_viridis_c(limits = limit, direction = -1)
@@ -30,7 +30,8 @@ plot_distance_heatmap <- function(phenotype_distances_result, metric = "mean"){
 
     }else if(metric == "std.dev"){
       limit <- range(unlist(phenotype_distances_result$Std.Dev), na.rm=TRUE)
-      g <- ggplot(phenotype_distances_result, aes(x = Reference, y = Nearest, fill = Std.Dev)) + geom_tile() + xlab("Cell of interest (COI)") + ylab("Nearest cell to COI") +
+      g <- ggplot(phenotype_distances_result, aes(x = Reference, y = Target, fill = Std.Dev)) + geom_tile() + 
+        xlab("Reference cell type") + ylab("Target cell type") +
         theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_rect(fill = "white"),
               axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
         scale_fill_viridis_c(limits = limit, direction = -1)
@@ -38,7 +39,24 @@ plot_distance_heatmap <- function(phenotype_distances_result, metric = "mean"){
 
     }else if(metric == "median"){
       limit <- range(unlist(phenotype_distances_result$Median), na.rm=TRUE)
-      g <- ggplot(phenotype_distances_result, aes(x = Reference, y = Nearest, fill = Median)) + geom_tile() + xlab("Cell of interest (COI)") + ylab("Nearest cell to COI") +
+      g <- ggplot(phenotype_distances_result, aes(x = Reference, y = Target, fill = Median)) + geom_tile() + 
+        xlab("Reference cell type") + ylab("Target cell type") +
+        theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_rect(fill = "white"),
+              axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
+        scale_fill_viridis_c(limits = limit, direction = -1)
+      print(g)
+    }else if(metric == "min"){
+      limit <- range(unlist(phenotype_distances_result$Min), na.rm=TRUE)
+      g <- ggplot(phenotype_distances_result, aes(x = Reference, y = Target, fill = Min)) + geom_tile() + 
+        xlab("Reference cell type") + ylab("Target cell type") +
+        theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_rect(fill = "white"),
+              axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
+        scale_fill_viridis_c(limits = limit, direction = -1)
+      print(g)
+    }else if(metric == "max"){
+      limit <- range(unlist(phenotype_distances_result$Max), na.rm=TRUE)
+      g <- ggplot(phenotype_distances_result, aes(x = Reference, y = Target, fill = Max)) + geom_tile() + 
+        xlab("Reference cell type") + ylab("Target cell type") +
         theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_rect(fill = "white"),
               axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
         scale_fill_viridis_c(limits = limit, direction = -1)
