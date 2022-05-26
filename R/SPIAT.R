f661e3e861ff1571a3cdd08b53d9eddcb76e441b
fuerzhou <653540853@qq.com>
2022-05-26 15:50:58 +1000

    add package level man page.

---
+++ R/SPIAT.R
@@ -0,0 +1,11 @@
+#' SPIAT: A package for spatial image analysis of tissues
+#'
+#' SPIAT includes six modules including reading sptial data, basic analysis and
+#' visualization, calculation of cell colocalization, the automated
+#' characterization of clinically relevant immune spatial patterns, measuring
+#' spatial heterogeneity and neighborhood analysis.
+#'
+#' @docType package
+#' @name SPIAT
+NULL
+#> NULL
