d22eff207b77f3285985c2f33e63a4c3fd795847
fuerzhou <653540853@qq.com>
2022-06-20 17:46:03 +1000

    Indicate using SpatialExperiment in package man page.

--- R/SPIAT.R
+++ R/SPIAT.R
@@ -1,9 +1,10 @@
 #' SPIAT: A package for spatial image analysis of tissues
 #'
-#' SPIAT includes six modules including reading sptial data, basic analysis and
+#' SPIAT has six modules including reading sptial data, basic analysis and
 #' visualization, calculation of cell colocalization, the automated
 #' characterization of clinically relevant immune spatial patterns, measuring
-#' spatial heterogeneity and neighborhood analysis.
+#' spatial heterogeneity and neighborhood analysis. It uses `SpatialExperiment`
+#' as the base class of analysis.
 #'
 #' @docType package
 #' @name SPIAT
