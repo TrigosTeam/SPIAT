989f94d17f1fd5ffe673ddd822da7a03d8e4739b
fuerzhou <653540853@qq.com>
2022-06-22 15:31:56 +1000

    adjust the indentation; shorten the lines

--- R/select_celltypes.R
+++ R/select_celltypes.R
@@ -17,7 +17,8 @@
 #'
 #' @examples
 #' data_subset <- select_celltypes(SPIAT::simulated_image,
-#' celltypes = c("Tumour_marker","Immune_marker1","Immune_marker2","Immune_marker3","Immune_marker4"),
+#' celltypes = c("Tumour_marker","Immune_marker1","Immune_marker2",
+#' "Immune_marker3","Immune_marker4"),
 #' feature_colname = "Phenotype", keep=TRUE)
 #' attr(data_subset, "original_cell_number") #cell number in the original image
 #' attr(data_subset, "range_of_coords")
@@ -25,32 +26,34 @@
 #' @export
 select_celltypes <- function(spe_object, celltypes, 
                              feature_colname = "Phenotype", keep = TRUE){
-  data <- get_colData(spe_object)
-  
-  # # if rownames have the cell types of interest, make rownames as a column
-  if (feature_colname == "Cell.ID"){
-    SummarizedExperiment::colData(spe_object)$Cell.ID <- data$Cell.ID
-  }
-  
-  # remember the total number of cells
-  n_cells <- dim(data)[1]
-  # remember the range of the cell coordinates
-  xmax <- max(data$Cell.X.Position)
-  xmin <- min(data$Cell.X.Position)
-  ymax <- max(data$Cell.Y.Position)
-  ymin <- min(data$Cell.Y.Position)
-  
-  # delete the cell rows
-  if (keep) slim_sce <- spe_object[, (spe_object[[feature_colname]] %in% celltypes)]
-  else slim_sce <- spe_object[, !(spe_object[[feature_colname]] %in% celltypes)]
-  
-  # # delete the rownames from the spe_object 
-  # SummarizedExperiment::colData(spe_object)$rownames <- NULL
-  
-  # save the original image info in the slim attr
-  attr(slim_sce, "original_cell_number") <- n_cells
-  attr(slim_sce, "range_of_coords") <- c(xmin = xmin, xmax = xmax, 
-                                         ymin = ymin, ymax = ymax)
-  
-  return(slim_sce)
+    data <- get_colData(spe_object)
+    
+    # # if rownames have the cell types of interest, make rownames as a column
+    if (feature_colname == "Cell.ID"){
+        SummarizedExperiment::colData(spe_object)$Cell.ID <- data$Cell.ID
+    }
+    
+    # remember the total number of cells
+    n_cells <- dim(data)[1]
+    # remember the range of the cell coordinates
+    xmax <- max(data$Cell.X.Position)
+    xmin <- min(data$Cell.X.Position)
+    ymax <- max(data$Cell.Y.Position)
+    ymin <- min(data$Cell.Y.Position)
+    
+    # delete the cell rows
+    if (keep) slim_sce <- 
+        spe_object[, (spe_object[[feature_colname]] %in% celltypes)]
+    else slim_sce <- 
+        spe_object[, !(spe_object[[feature_colname]] %in% celltypes)]
+    
+    # # delete the rownames from the spe_object 
+    # SummarizedExperiment::colData(spe_object)$rownames <- NULL
+    
+    # save the original image info in the slim attr
+    attr(slim_sce, "original_cell_number") <- n_cells
+    attr(slim_sce, "range_of_coords") <- c(xmin = xmin, xmax = xmax, 
+                                           ymin = ymin, ymax = ymax)
+    
+    return(slim_sce)
 }
